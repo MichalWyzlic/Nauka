1acb832bc02481cc1c8510751c6771b71c78587f
MichalWyzlic <michal.wyzlic@lennoxemea.com>
2020-12-12 15:07:31 +0100

    JavaScript30

---
+++ JavaScript Course/basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,15 @@
+//Function definition 
+
+function add(number1, number2){
+    return number1 + number2;
+}
+
+
+
+currentResult = add(1, 2);
+
+let calculationDescription = '( '+ currentResult + ' + 10) * 3 / 2  -1';
+
+
+outputResult(currentResult, calculationDescription);
+
