a5861be297436d9efd023b5e03bc8968c0ac8996
MichalWyzlic <michal.wyzlic@lennoxemea.com>
2020-12-22 17:17:07 +0100

    End of section Calculator

--- JavaScript Course/basics-01-starting-project/assets/scripts/app.js
+++ JavaScript Course/basics-01-starting-project/assets/scripts/app.js
@@ -1,15 +1,79 @@
-//Function definition 
+//Function definition
+const defaultValue = 0;
+let currentResult = defaultValue;
+let logEntries = [];
 
-function add(number1, number2){
-    return number1 + number2;
+function getUserNumberInput(){
+    return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator, result, resultBeforeCalc, calcNumber){
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(result, calcDescription);
+    const logEntry = {
+        operation: operator,
+        prevResult: resultBeforeCalc,
+        number: calcNumber,
+        result: result
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries[logEntries.length-1]);
+}
+
+function writeToLog(operator, result, resultBeforeCalc, calcNumber){
+    const logEntry = {
+        operation: operator,
+        prevResult: resultBeforeCalc,
+        number: calcNumber,
+        result: result
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries[logEntries.length-1]);
+}
+
+function add(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult += enteredNumber;
+
+    createAndWriteOutput("+", currentResult, initialResult, enteredNumber);
+    writeToLog("+", currentResult, initialResult, enteredNumber);
+ 
+}
+
+function subtract(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult -= enteredNumber;
+    createAndWriteOutput("-", currentResult, initialResult, enteredNumber);
+    writeToLog("-", currentResult, initialResult, enteredNumber);
+}
+
+function multiply(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult *= enteredNumber;
+    createAndWriteOutput("*", currentResult, initialResult, enteredNumber);
+    writeToLog("*", currentResult, initialResult, enteredNumber);
 }
 
 
+function divide(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult /= enteredNumber;
+    createAndWriteOutput("/", currentResult, initialResult, enteredNumber);
+    writeToLog("/", currentResult, initialResult, enteredNumber);
+}
 
-currentResult = add(1, 2);
 
-let calculationDescription = '( '+ currentResult + ' + 10) * 3 / 2  -1';
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
 
 
-outputResult(currentResult, calculationDescription);
+
+
+
 
