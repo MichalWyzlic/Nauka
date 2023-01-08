8c100c22c345f3b6fac8515c46fa12b6dd44675d
MichalWyzlic <michal.wyzlic@lennoxemea.com>
2023-01-08 16:06:21 +0100

    JS 154 done

---
+++ JavaScript Course/basics-01-starting-project/New DOM 20230106 L147/app.js
@@ -0,0 +1,25 @@
+const h1 =  document.getElementById('main-title');
+
+const changeTitle = () => {
+	h1.textContent = 'Content after a click';
+	h1.style.color = 'white';
+	h1.style.backgroundColor = 'black';
+}
+
+const li = document.querySelector('li:last-of-type');
+
+const changeLi = () => {
+	li.textContent = li.textContent + ' (Changed!)';
+	li.style.color = 'red';
+	li.style.backgroundColor = 'green';
+}
+
+document.addEventListener('click', changeTitle);
+document.addEventListener('keyup', changeLi);
+// const listItemElements = document.querySelectorAll('li');
+
+const listItemElements = document.getElementsByTagName('li');
+
+for (const listItemEl of listItemElements){
+	console.dir(listItemEl);
+}
