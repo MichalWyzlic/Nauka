c19422c9eb2588be026f8dbd64efdb3e64893850
Michal Wyzlic <wyzlic.michal@gmail.com>
2023-01-17 23:39:07 +0100

    JS 189 done

---
+++ JavaScript Course/arrays-01-starting-code/app.js
@@ -0,0 +1,17 @@
+// const hobbies = ['Sports', 'Movies'];
+// hobbies.push('Reading');
+// hobbies.unshift('Coding');
+// console.log(hobbies);
+// hobbies.pop();
+// console.log(hobbies);
+// hobbies.shift();
+// console.log(hobbies);
+
+// hobbies.splice(1, 0, 'Good food.');
+// console.log(hobbies);
+
+const results = [1, 2.5, 3, 4.2, 15]
+const newResults = results.slice(0,3);
+
+results.push(5.22);
+console.log(results,newResults);
