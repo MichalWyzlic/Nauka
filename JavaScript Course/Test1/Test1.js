a8d7a4b9b3952af0834eb496eca8a45c1f0d71d1
Michal Wyzlic <wyzlic.michal@gmail.com>
2023-01-22 16:54:57 +0100

    JS 204 done

--- JavaScript Course/Test1/Test1.js
+++ JavaScript Course/Test1/Test1.js
@@ -1,1 +1,93 @@
-const 
+const scores = {
+	'Emy': [12],
+	'Sandra': [1,1,1,1,1],
+	'Sue': [7]
+};
+
+function podium(scores){
+	//Check the scores table
+	const verifiedScores = Object.entries(scores);
+
+	//create an array of competitors and their cumulated scores
+	const calculatedScores = Object.entries(scores).map((value) => {
+		const points = value[1];
+		let sumOfPoints = undefined;
+		points.forEach(element => {
+			if(sumOfPoints === undefined){
+				sumOfPoints = 0;
+			}
+			sumOfPoints += element;			
+		});
+		return [value[0], sumOfPoints]
+	});
+
+	//reduce empty elements
+	for(let i = 0; i < calculatedScores.length; i++){
+		while(calculatedScores[i][0] === '' || calculatedScores[i][1] === undefined){
+			calculatedScores.splice(i,1);
+			if(i >= calculatedScores.length){
+				break;
+			};
+		};
+	};
+
+	//sort in a descending order
+	calculatedScores.sort((a,b) => b[1]-a[1]);
+
+	//if no valid scores - return an empty object
+	if(calculatedScores.length === 0){
+		return {};
+	}
+
+	function checkPosition(){
+		let position = {
+			'score': 0,
+			'players': []
+		  };
+		position.score = calculatedScores[index][1];
+		position.players.push(calculatedScores[index][0]);
+		index ++;
+		//check if more than one silver medal
+		if(index < calculatedScores.length){
+			while(position.score === calculatedScores[index][1]){
+				position.players.push(calculatedScores[index][0]);
+				index ++;
+				if(index === calculatedScores.length){
+					break;
+				};
+			};
+		};
+		position.players.sort();
+
+		return position;
+	}
+
+	//check for gold
+	let podium =  {};
+	let index = 0;
+	let goldPosition = checkPosition();
+	if(goldPosition.players != []){
+		podium.gold = goldPosition;
+	}
+
+	
+	// check for silver medals (only one gold)
+	if(index < 2 && index < calculatedScores.length){
+		let silverPosition = checkPosition();
+		if(silverPosition.players != []){
+			podium.silver = silverPosition;
+		}
+	};
+
+	// check for bronze medals (only one gold and silver)
+	if(index < 3 && index < calculatedScores.length){
+		let bronzePosition = checkPosition();
+		if(bronzePosition.players != []){
+			podium.bronze = bronzePosition;
+		}
+	};
+
+	return podium;
+};
+
+console.log(podium(scores));
--- JavaScript Course/arrays-01-starting-code/app.js
+++ JavaScript Course/arrays-01-starting-code/app.js
@@ -10,8 +10,28 @@
 // hobbies.splice(1, 0, 'Good food.');
 // console.log(hobbies);
 
-const results = [1, 2.5, 3, 4.2, 15]
-const newResults = results.slice(0,3);
+// const results = [1, 2.5, 3, 4.2, 15]
+// const newResults = results.slice(0,3);
 
-results.push(5.22);
-console.log(results,newResults);
+// results.push(5.22);
+// console.log(results,newResults)
+
+// const nameFragments = ['Max', 'Schwartz'];
+// const name1 = nameFragments.join(' ');
+// console.log(name1);
+
+// const copiedNameFragments = [...nameFragments];
+// nameFragments.push('Mr');
+// console.log(nameFragments, copiedNameFragments);
+
+// const persons = [{name: 'Max', age: 30}, {name: 'Manuel', age: 31}];
+// const copiedPersons = [...persons];
+// persons.push({name: 'Tom', age: 15});
+// persons[0].age = 44;
+
+// console.log(persons, copiedPersons);
+
+const nameFragments = ['Max', 'Schwarz', 'Mr', 30];
+const [firstName, lastName, ...otherInformation] = nameFragments;
+console.log(firstName, lastName, otherInformation);
+
