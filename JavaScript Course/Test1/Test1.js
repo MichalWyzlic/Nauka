de5ee13b111c68dd3957ae81c72208c9f7c1c256
MichalWyzlic <michal.wyzlic@lennoxemea.com>
2023-01-23 16:12:24 +0100

    JS 212 done

--- JavaScript Course/Test1/Test1.js
+++ JavaScript Course/Test1/Test1.js
@@ -1,93 +1,34 @@
-const scores = {
-	'Emy': [12],
-	'Sandra': [1,1,1,1,1],
-	'Sue': [7]
+const numbers = [1, 2, 15.22, 35, 4.99, 5.01, 7, 0.15, 13.02];
+console.log(numbers);
+
+const filteredNumbers = numbers.filter(value =>  value > 5 );
+console.log(filteredNumbers);
+
+numbers[3]= 15.33;
+console.log(numbers);
+console.log(filteredNumbers);
+
+const newNumbers = numbers.map((value, index) => ({index: index, value: value}));
+console.log(newNumbers);
+
+const reducedArray = numbers.reduce((previousValue, currentValue) => previousValue * currentValue);
+console.log(reducedArray);
+
+function findMax(...listOfNumbers){
+	return [Math.min(...listOfNumbers), Math.max(...listOfNumbers)];
 };
 
-function podium(scores){
-	//Check the scores table
-	const verifiedScores = Object.entries(scores);
+const [arrayMin, arrayMax] = findMax(...numbers);
+console.log(findMax(...numbers), arrayMin, arrayMax);
 
-	//create an array of competitors and their cumulated scores
-	const calculatedScores = Object.entries(scores).map((value) => {
-		const points = value[1];
-		let sumOfPoints = undefined;
-		points.forEach(element => {
-			if(sumOfPoints === undefined){
-				sumOfPoints = 0;
-			}
-			sumOfPoints += element;			
-		});
-		return [value[0], sumOfPoints]
-	});
+const listNoDuplicates = new Set(['Hi', 'it', 'is']);
+console.log(listNoDuplicates.size);
+listNoDuplicates.add('me');
+console.log(listNoDuplicates.size);
+listNoDuplicates.add('Hi');
+console.log(listNoDuplicates.size);
 
-	//reduce empty elements
-	for(let i = 0; i < calculatedScores.length; i++){
-		while(calculatedScores[i][0] === '' || calculatedScores[i][1] === undefined){
-			calculatedScores.splice(i,1);
-			if(i >= calculatedScores.length){
-				break;
-			};
-		};
-	};
+listNoDuplicates.add('Michal.');
+console.log(listNoDuplicates.size);
 
-	//sort in a descending order
-	calculatedScores.sort((a,b) => b[1]-a[1]);
-
-	//if no valid scores - return an empty object
-	if(calculatedScores.length === 0){
-		return {};
-	}
-
-	function checkPosition(){
-		let position = {
-			'score': 0,
-			'players': []
-		  };
-		position.score = calculatedScores[index][1];
-		position.players.push(calculatedScores[index][0]);
-		index ++;
-		//check if more than one silver medal
-		if(index < calculatedScores.length){
-			while(position.score === calculatedScores[index][1]){
-				position.players.push(calculatedScores[index][0]);
-				index ++;
-				if(index === calculatedScores.length){
-					break;
-				};
-			};
-		};
-		position.players.sort();
-
-		return position;
-	}
-
-	//check for gold
-	let podium =  {};
-	let index = 0;
-	let goldPosition = checkPosition();
-	if(goldPosition.players != []){
-		podium.gold = goldPosition;
-	}
-
-	
-	// check for silver medals (only one gold)
-	if(index < 2 && index < calculatedScores.length){
-		let silverPosition = checkPosition();
-		if(silverPosition.players != []){
-			podium.silver = silverPosition;
-		}
-	};
-
-	// check for bronze medals (only one gold and silver)
-	if(index < 3 && index < calculatedScores.length){
-		let bronzePosition = checkPosition();
-		if(bronzePosition.players != []){
-			podium.bronze = bronzePosition;
-		}
-	};
-
-	return podium;
-};
-
-console.log(podium(scores));
+console.log(listNoDuplicates);
