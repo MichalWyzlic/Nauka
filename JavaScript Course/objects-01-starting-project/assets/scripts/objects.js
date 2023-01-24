5b8d908040b4001388f0f69a96c474f58585f629
MichalWyzlic <michal.wyzlic@lennoxemea.com>
2023-01-24 14:30:29 +0100

    JS 223 done

--- JavaScript Course/objects-01-starting-project/assets/scripts/objects.js
+++ JavaScript Course/objects-01-starting-project/assets/scripts/objects.js
@@ -1,22 +1,69 @@
-const movieList = document.getElementById('movie-list');
+const addMovieButton = document.getElementById('add-movie-btn');
+const searchButton = document.getElementById('search-btn');
 
-movieList.style['background-color'] = 'green';
-movieList.style.display = 'block'; 
+const movies = [];
 
-const person = {
-	'first name': 'Max',
-	age: 30,
-	hobbies: ['Sports', 'Cooking'],
-	greet: function() {
-		alert(`Hi there, I am ${this.name} and I am ${this.age} years old.`);
-	},
-	1.5: 'hello'
+const renderMovies = (filter = '') => {
+	const movieList = document.getElementById('movie-list');
+
+	
+	if(movies.length === 0){
+		movieList.classList.remove('visible');
+		return;
+	} else {
+		movieList.classList.add('visible');
+	};
+
+	movieList.innerHTML = '';
+
+	const filteredMovies = !filter 
+		? movies 
+		: movies.filter( movie => movie.info.title.includes(filter));
+
+	filteredMovies.forEach((movie) => {
+		const movieEl = document.createElement('li');
+		let text = movie.info.title + ' - ';
+		for (const key in movie.info){
+			if(key !== 'title'){
+				text += `${key}: ${movie.info[key]}`;
+			}
+		}
+
+		movieEl.textContent =  text;
+		movieList.append(movieEl);
+	});
+
 };
 
-delete person.age;
-//person.age = undefined;
-person.isAdmin = true;
+const addMovieHandler = () => {
+	const title = document.getElementById('title').value;
+	const extraName = document.getElementById('extra-name').value;
+	const extraValue = document.getElementById('extra-value').value;
 
-console.log(person);
+	if(
+		title.trim() === '' 
+		|| extraName.trim() ===  ''
+		|| extraValue.trim() === ''
+	){
+		return;
+	};
 
+	const newMovie = {
+		info: {
+			title,
+			[extraName]: extraValue
+		},
+		id: Math.random()
+	}
 
+	movies.push(newMovie);
+	renderMovies();
+};
+
+const searchMovieHandler = () => {
+	const filterTerm = document.getElementById('filter-title').value;
+	renderMovies(filterTerm);
+};
+
+addMovieButton.addEventListener('click', addMovieHandler);
+searchButton.addEventListener('click', searchMovieHandler);
