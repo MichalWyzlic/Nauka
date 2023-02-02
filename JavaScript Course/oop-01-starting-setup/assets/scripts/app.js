e715004f0bfaa5e0265b6e799c4738a1cb9b94c9
MichalWyzlic <michal.wyzlic@lennoxemea.com>
2023-02-02 14:36:07 +0100

    JS 246 done

--- JavaScript Course/oop-01-starting-setup/assets/scripts/app.js
+++ JavaScript Course/oop-01-starting-setup/assets/scripts/app.js
@@ -11,9 +11,39 @@ class Product{
 		this.price = price;
 	}
 };
-console.log(new Product());
-const productList = {
-	products: [
+
+class ProductItem{
+	constructor(product){
+		this.product = product;
+	}
+
+	addToCartHandler(){
+		console.log('Adding product to cart...');
+		console.log(this.product);
+	}
+
+	render(){
+		const prodEl = document.createElement('li');
+		prodEl.className = 'product-item';
+		prodEl.innerHTML =`
+			<div>
+				<img src="${this.product.imageUrl}" alt="${this.product.title}">
+				<div class="product-item__content">
+					<h2>${this.product.title}</h2>
+					<h3>\$${this.product.price}</h3>
+					<p>${this.product.description}</p>
+					<button>Add to Cart</button>
+				</div>
+			</div>
+			`;
+		const addCartButton = prodEl.querySelector('button');
+		addCartButton.addEventListener('click', this.addToCartHandler.bind(this));
+		return prodEl;
+	};
+};
+
+class ProductList {
+	products = [
 		new Product('A Pillow',
 					'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.04Ttowe8HEE2Ob7xmxyxRgHaEJ%26pid%3DApi&f=1&ipt=cc717073e1648e6b629fa211f55bb4aba05245dc0fcb7a99104950bb97014074&ipo=images',
 					'A soft pillow!',
@@ -24,32 +54,23 @@ const productList = {
 					'A carpet that you might like - or not.',
 					89.99
 					)
-	],
+	];
+
+	constructor(){};
 
 	render() {
 		const renderHook = document.getElementById('app');
 		const prodList = document.createElement('ul');
 		prodList.className = 'product-list';
 		for(const prod of this.products){
-			const prodEl = document.createElement('li');
-			prodEl.className = 'product-item';
-			prodEl.innerHTML =`
-			<div>
-				<img src="${prod.imageUrl}" alt="${prod.title}">
-				<div class="product-item__content">
-					<h2>${prod.title}</h2>
-					<h3>\$${prod.price}</h3>
-					<p>${prod.description}</p>
-					<button>Add to Cart</button>
-				</div>
-			</div>
-			`;
+			const productItem = new ProductItem(prod);
+			const prodEl = productItem.render();
 			prodList.append(prodEl);
 		};
-
 		renderHook.append(prodList);
-	}
-
+	};
 };
 
+
+const productList = new ProductList();
 productList.render();
