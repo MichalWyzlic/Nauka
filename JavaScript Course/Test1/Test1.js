ddd3c32a904d640a554058b93f74458f9a408d74
Michal Wyzlic <wyzlic.michal@gmail.com>
2023-02-10 22:29:36 +0100

    Kata NamedOne

--- JavaScript Course/Test1/Test1.js
+++ JavaScript Course/Test1/Test1.js
@@ -1,35 +1,151 @@
 'use strict';
-function possibilities(str) {
-	let positions = [];
-	let order = 0;
-	let number = [];
-	let result = [];
-	for(let i = str.length - 1; i >= 0; i--){
-		if(str[i] === '?'){
-			positions.push(i);
-			order ++;
-			number[i]='0';
+
+class Bike{
+	constructor(license){
+		this.license = license;
+	}
+}
+
+class Car{
+	constructor(license){
+		this.license = license;
+	}
+}
+
+class Van{
+	constructor(license){
+		this.license = license;
+	}
+}
+
+class ParkingLot {
+	constructor(size) {
+		this.parking = Array(size).fill('0').join(',');
+		this.size = size;
+	}
+	park(vehicle) {
+		// const regEx = new RegExp(`${vehicle.license}`,'g'); 
+		// if(regEx.test(this.parking)){
+		// 	return false;
+		// };
+
+		if(vehicle instanceof Bike){
+			if(/0/.test(this.parking)){
+				this.parking = this.parking.replace(/0/,`${vehicle.license}`);
+				return true;
+			} else {
+				return false;
+			}
+
+		} else if(vehicle instanceof Car){
+			if(/0,0/.test(this.parking)){
+				this.parking = this.parking.replace(/0,0/,`${vehicle.license},${vehicle.license}`);
+				return true;
+			} else {
+				return false;
+			}
+
+		}else if(vehicle instanceof Van){
+			if(/0,0,0/.test(this.parking)){
+				this.parking = this.parking.replace(/0,0,0/,`${vehicle.license},${vehicle.license},${vehicle.license}`);
+				return true;
+			} else {
+				return false;
+			}
+
 		} else {
-			number[i]=str[i];
+			return false;
 		}
 	}
+	retrieve(license) {
+		const parkingArray = this.parking.split(',');
+		let result = false;
+		parkingArray.forEach(function test(value, index, arr){
+			if(value === license){
+				arr[index] = 0;
+				result = true;
+			};
+		});
+		this.parking = parkingArray.join(',');
 
-	result.push(number.join(''));
+		return result;
 
-	for(let i = 1; i <= order; i++){
-		const binNumber = i.toString(2);
-		const tempArray = [...number];
-		for(let j = 0; j < binNumber.length; j++){
-			tempArray[positions[j]] = binNumber[j];
-		};
-		result.push(tempArray.join(''));
+		// const regEx = new RegExp(`${license}`,'g'); 
+		// if(regEx.test(this.parking)){
+		// 	this.parking = this.parking.replace(regEx,`0`);
+		// 	return true;
+		// } else {
+		// 	return false;
+		// }
 	}
+}
 
-	return result;
-  };
-const testAnArray = [1,2]
 
-console.log(possibilities('101?'));
-console.log(possibilities('10??01?'));
-console.log(possibilities('1?1?'));
+
+let testParking = new ParkingLot(4);
+const bike1 = new Bike("BK-123");
+const bike2 = new Bike("BK-456");
+const bike3 = new Bike("BK-789");
+const car1 = new Car("CR-123");
+const car2 = new Car("CR-456");
+const van1 = new Van("VN-123");
+const van2 = new Van("VN-456");
+console.log(testParking.park(bike1));
+console.log(testParking.park(car1));
+console.log(testParking.park(bike2))
+console.log(testParking.park(van1));
+console.log(testParking.retrieve(bike1.license));
+console.log(testParking.retrieve(bike2.license));
+console.log(testParking.park(car2));
+
+//console.log(testParking.retrieve(bike1.license));
+console.log(testParking.park(bike3));
+console.log(testParking.park(car2));
+console.log(testParking.park(van2));
+// console.log(testParking.retrieve(car2.license));
+// console.log(testParking.retrieve(van1.license));
+// console.log(testParking.park(car2));
+
+function NamedOne(first, last) {
+	// -- SHOULD be changed --
+	const _firstName = first;
+	const _lastName = last;
+	Object.defineProperty(this,"firstName",{
+		get: function() { return _firstName; },
+		set: function(fName) { 
+			if(typeof _firstName === 'undefined'){
+				_firstName = fName;
+			}
+		 }
+	  });
+	  Object.defineProperty(this,"lastName",{
+		get: function() { return _lastName },
+		set: function(lName) { 
+			if(typeof _lastName === 'undefined'){
+				_lastName = lName;
+			}
+		 }
+	  });
+	Object.defineProperty(this,"fullName",{
+		get: function() { return _firstName + ' ' + _lastName; },
+		set: function(fName) { 
+			const tempArray = fName.split(' ');
+			if(tempArray.length === 2 && typeof _firstName === 'undefined' && typeof _lastName === 'undefined'){
+				_firstName = tempArray[0];
+				_lastName = tempArray[1];
+			}
+		 }
+	  });
+	// this.fullName = this.firstName + ' ' + this.lastName;
+}
+
+
+
+let n;
+n=new NamedOne('John', 'Doe');
+console.log(n.firstName, n.lastName, n.fullName);
+n.lastName = 'Wojdylo';
+console.log(n.firstName, n.lastName, n.fullName);
+
+
 
