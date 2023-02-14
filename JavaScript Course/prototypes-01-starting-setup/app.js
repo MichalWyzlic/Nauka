c3037795da13c60b690f403a519fad50f9bbbc9d
MichalWyzlic <michal.wyzlic@lennoxemea.com>
2023-02-14 14:18:51 +0100

    JS next

---
+++ JavaScript Course/prototypes-01-starting-setup/app.js
@@ -0,0 +1,16 @@
+function Person() {
+	this.name = 'Max';
+
+	this.age = 30;
+	
+
+	this.greet = () => {
+		console.log('Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.');
+	};
+}
+
+const person = new Person();
+person.greet();
+console.dir(Person);
+console.dir(Person.__proto__);
+console.dir(Person.prototype);
