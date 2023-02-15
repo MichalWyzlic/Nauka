7095e68754fbef0e3608dd60f655a6225d61366d
MichalWyzlic <michal.wyzlic@lennoxemea.com>
2023-02-15 15:13:06 +0100

    JS 278 done

--- JavaScript Course/prototypes-01-starting-setup/app.js
+++ JavaScript Course/prototypes-01-starting-setup/app.js
@@ -1,16 +1,30 @@
-function Person() {
-	this.name = 'Max';
+// function Person() {
+// 	this.name = 'Max';
 
-	this.age = 30;
+// 	this.age = 30;
 	
 
-	this.greet = () => {
-		console.log('Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.');
-	};
-}
+// 	this.greet = () => {
+// 		console.log('Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.');
+// 	};
+// }
 
-const person = new Person();
-person.greet();
-console.dir(Person);
-console.dir(Person.__proto__);
-console.dir(Person.prototype);
+// const person = new Person();
+// person.greet();
+// console.dir(Person);
+// console.dir(Person.__proto__);
+// console.dir(Person.prototype);
+
+
+const course = {
+	title: 'JavaScript - The Complete Guide',
+	rating: 5
+};
+
+Object.setPrototypeOf(course, {
+	printRating: function() {
+		(`${this.rating}/5`);
+	}
+});
+
+course.printRating();
