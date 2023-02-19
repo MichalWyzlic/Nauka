4feba2fcfb108d6d30dadbdea1e07707fffa69f9
Michal Wyzlic <wyzlic.michal@gmail.com>
2023-02-19 10:57:42 +0100

    hackerrank day2 challenge

--- JavaScript Course/Test1/Test1.js
+++ JavaScript Course/Test1/Test1.js
@@ -1,139 +1,79 @@
-const  words = ['aab', 'defgab', 'abcde', 'aabcde', 'bbbbbbbbbb', 'jabjjjad'];
+matrix = [[112, 42, 83, 119], [56, 125, 56, 49], [15, 78, 101, 43], [62, 98, 114, 108]];
 
-    // // Write your code here
-	// for(let i = 0; i < words.length; i++){
-	// 	for(let j = 0; j < words.length; j++){
-	// 		if(j !== i && words[i].length >= words[j].length){
-	// 			if(words[j] === words[i].substring(0, words[j].length)){
-	// 				console.log('BAD SET');
-	// 				console.log(words[i]);
-	// 				return;
-	// 			};
-	// 		};
-	// 	};
-	// };
-	// console.log('GOOD SET');
-
-
-class TrieNode {
-	constructor() {
-		this.keys = new Map();
-		this.end = false;
-	};
-
-	setEnd(){
-		this.end = true;
-	};
-
-	isEnd(){
-		return this.end;
-	};
+function checkMaxValuePosition(pi, pj, matrix){
+	let n = matrix.length - 1;
+	let tempArray = [[matrix[pi][pj],1],[matrix[pi][n-pj],2],[matrix[n-pi][n-pj],3],[matrix[n-pi][pj],4]];
+	tempArray.sort((a,b) => b[0] - a[0]);
+	return tempArray[0][1];
 };
 
-class Trie{
-	constructor(){
-		this.root = new TrieNode();
+function reverseV(c, matrix){
+	let n = matrix.length - 1;
+	if(c > n){
+		return;
 	};
-
-	add(input, node = this.root){
-		if(input.length === 0){
-			node.setEnd();
-			return;
-		} else if(!node.keys.has(input[0])){
-			node.keys.set(input[0], new TrieNode());
-			return this.add(input.substring(1), node.keys.get(input[0]));
-		}else {
-			return this.add(input.substring(1), node.keys.get(input[0]));
-		};
-	};
-
-	isWord(word){
-		let node = this.root;
-		while(word.length > 1){
-			if(!node.keys.has(word[0])){
-				return false;
-			} else{
-				node = node.keys.get(word[0]);
-				word = word.substring(1);
-			};
-		};
-
-		return (node.keys.has(word) && node.keys.get(word).isEnd());
-	};
-
-
-
-	print(){
-		const words = [];
-		const search = function(node = this.root, string){
-			if(node.keys.size !== 0){
-				for(let letter of node.keys.keys()){
-					search(node.keys.get(letter), string.concat(letter));
-				};
-				if(node.isEnd()){
-					words.push(string);
-				};
-			} else{
-				string.length > 0 ? words.push(string) : undefined;
-				return;
-			};
-		};
-		search(this.root, new String());
-		return words.length > 0 ? words : null;
+	for(let i = 0; i < matrix.length/2; i++){
+		let tempValue = matrix[i][c];
+		matrix[i][c] = matrix[n-i][c];
+		matrix[n-i][c] = tempValue;
 	};
 };
 
 
-function noPrefix(words) {
 
-
-	const testTrie = new Trie();
-	for(let i = 0; i<words.length; i++){
-		if(i > 0){
-
-			let isPrefix = false;
-			let hasPrefix = false;
-			let node = testTrie.root;
-			let word = words[i];
-			while(word.length > 1){
-				if(!node.keys.has(word[0])){
+function flippingMatrix(matrix) {
+	let n = matrix.length / 2 - 1;
+	let n2 = matrix.length - 1;
+	let sum = 0;
+	for(let i = 0; i <= n; i ++){
+		for(let j = 0; j <= n; j++){
+			let maxPosition = checkMaxValuePosition(i, j, matrix);
+			switch(maxPosition){
+				case 2 :
+					reverseV(n2-j, matrix);
+					matrix[i].reverse();
+					reverseV(n2-j, matrix);
+					matrix[i].reverse();
 					break;
-				} else{
-					node = node.keys.get(word[0]);
-					word = word.substring(1);
-					if(node.isEnd()){
-						hasPrefix = true;
-					}
-				};
+				case 3:
+					matrix[i].reverse();
+					reverseV(n2-j, matrix);
+					matrix[i].reverse();
+					break;
+				case 4:
+					matrix[i].reverse();
+					matrix[n2-i].reverse();
+					reverseV(n2-j, matrix);
+					matrix[i].reverse();
+					break;
 			};
-	
-			isPrefix = node.keys.has(word);
-
-			if(isPrefix || hasPrefix){
-				console.log('BAD SET\n' + words[i]);
-				return;
-			}
+			sum += matrix[i][j];
 		};
-		testTrie.add(words[i]);
 	};
-	console.log('GOOD SET');
+
+	return sum;
+	
+};
+reverseV(2,matrix);
+console.log(flippingMatrix(matrix));
+
+
+function fizzBuzz(n) {
+    // Write your code here
+	for(let i = 1; i <= n; i++){
+		let output = '';
+		if(Math.floor(i/3)===i/3){
+			output += 'Fizz';
+		};
+		if(Math.floor(i/5)===i/5){
+			output += 'Buzz';
+		};
+		if(output === ''){
+			output += i;
+		};
+		console.log(output);
+	};
 
 };
 
-// myTrie = new Trie();
-// myTrie.add('ball');
-// myTrie.add('dorm');
-// myTrie.add('send');
-// myTrie.add('bat');
-// myTrie.add('do');
-// myTrie.add('sense');
-// myTrie.add('doll');
-// myTrie.add('dork');
-// myTrie.add('sensei');
-
-// console.log(myTrie.isWord('doll'));
-// console.log(myTrie.isWord('sen'));
-// console.log(myTrie.isWord('ball'));
-// console.log(myTrie.print());
-
-noPrefix(words);
+fizzBuzz(15);
