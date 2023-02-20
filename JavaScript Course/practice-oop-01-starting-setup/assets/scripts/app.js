c7cbc440015419928c1dd531f3b74e42ebe258b2
MichalWyzlic <michal.wyzlic@lennoxemea.com>
2023-02-20 15:40:38 +0100

    JS 287 done

--- JavaScript Course/practice-oop-01-starting-setup/assets/scripts/app.js
+++ JavaScript Course/practice-oop-01-starting-setup/assets/scripts/app.js
@@ -5,34 +5,105 @@ class DOMHelper{c
 		return clonedElement;
 	}
 
-	static moveElement(elementId, newDestiantionSelector){
+	static moveElement(elementId, newDestinationSelector){
 		const element = document.getElementById(elementId);
-		const destinationElement = document.querySelector(newDestiantionSelector);
+		const destinationElement = document.querySelector(newDestinationSelector);
 		destinationElement.append(element);
 
 	};
 };
 
-class Tooltip {};
+
+class Component {
+	constructor(hostElementId, insertBefore = false){
+		if(hostElementId){
+			this.hostElement = document.getElementById(hostElementId);
+		} else {
+			this.hostElement = document.body;
+		};
+
+		this.insertBefore = insertBefore;
+	}
+
+	detach () {
+		if(this.element){
+			this.element.remove();
+		};
+	};
+
+	attach(){
+		this.hostElement.insertAdjacentElement(
+			this.insertBefore ? 'afterbegin' : 'beforeend', 
+			this.element
+			);
+	};
+}
+
+
+class Tooltip extends Component{
+	constructor(closeNotifierFunction){
+		super('active-projects', true);
+		this.closeNotifierHandler = closeNotifierFunction;
+		this.create();
+	};
+
+	closeTooltip = () => {
+		this.detach();
+		this.closeNotifierHandler();
+
+	};
+
+	create() {
+		console.log('The tooltip...');
+		const toolTipEl = document.createElement('div');
+		toolTipEl.className = 'card';
+		toolTipEl.textContent = 'DUMMY!';
+		this.element = toolTipEl;
+		toolTipEl.addEventListener('click', this.closeTooltip);
+	};
+
+
+};
+
 
 class ProjectItem {
-	constructor(id, updateProjectListsFunction){
+	hasActiveTooltip = false;
+
+	constructor(id, updateProjectListsFunction, type){
 		this.updateProjectListsHandler = updateProjectListsFunction;
 		this.id = id;
 		this.connectMoreInfoButton();
-		this.connectSwitchButton();
+		this.connectSwitchButton(type);
 	}
 
-	connectMoreInfoButton(){};
+	showMoreInfoHandler(){
+		if(this.hasActiveTooltip){
+			return;
+		}
+		const toolTip = new Tooltip(() => {this.hasActiveTooltip = false});
+		toolTip.attach();
+		this.hasActiveTooltip = true;
+	}
 
-	connectSwitchButton(){
+	connectMoreInfoButton(){
+		let moreInfoBtn = document.getElementById(this.id).querySelector('button:first-of-type');
+		moreInfoBtn = DOMHelper.clearEventListener(moreInfoBtn);
+		moreInfoBtn.addEventListener('click', this.showMoreInfoHandler);
+	};
+
+	connectSwitchButton(type){
 		let switchButtonEl = document.getElementById(this.id).querySelector('button:last-of-type');
 		switchButtonEl = DOMHelper.clearEventListener(switchButtonEl);
-		switchButtonEl.addEventListener('click', this.updateProjectListsHandler);
+		switchButtonEl.textContent = type === 'active' ? 'Finish' : 'Activate';
+		switchButtonEl.addEventListener(
+			'click', 
+			this.updateProjectListsHandler.bind(null, this.id)
+		);
 	};
 
 	update(updateProjectListsFn, type){
 		this.updateProjectListsHandler = updateProjectListsFn;
+		this.connectSwitchButton(type);
 		this.type = type;
 	}
 };
@@ -43,7 +114,9 @@ class ProjectList {
 		this.type = type;
 		const prjItems = document.querySelectorAll(`#${type}-projects li`);
 		for(const prjItem of prjItems){
-			this.projects.push(new ProjectItem(prjItem.id, this.switchProject.bind(this)));
+			this.projects.push(
+				new ProjectItem(prjItem.id, this.switchProject.bind(this), type)
+				);
 		}
 		console.log(this.projects);
 
@@ -60,8 +133,10 @@ class ProjectList {
 	};
 
 	switchProject(projectId){
-		const projectIndex = this.projects.findIndex(p => p.id === projectId);
-		this.switchHandler(this.projects.splice(projectIndex,1)[0]);
+		// const projectIndex = this.projects.findIndex(p => p.id === projectId);
+		// this.switchHandler(this.projects.splice(projectIndex,1)[0]);
+		this.switchHandler(this.projects.find(p => p.id === projectId));
+		this.projects = this.projects.filter(p => p.id !== projectId);
 	};
 };
 
