7370391850b5372247d02e8f2c27e8bb21b11d6d
MichalWyzlic <michal.wyzlic@lennoxemea.com>
2023-03-03 21:19:30 +0100

    codewars

--- JavaScript Course/async-01-starting-setup/app.js
+++ JavaScript Course/async-01-starting-setup/app.js
@@ -1,8 +1,51 @@
+'use strict'
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+const getPosition = (opts) => {
+	const promise = new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(success => {
+			resolve(success);
+		}, error => {
+			reject(error);
+		}, opts);
+	});
+
+	return promise;
+	
+}
+
+const setTimer = (duration) => {
+	const promise = new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve('Done!');
+		}, duration);
+	});
+
+	return promise;
+};
+
+
+
+
 function trackUserHandler() {
-  console.log('Clicked!');
+	let positionData;
+	getPosition()
+		.then(posData => {
+			positionData = posData;
+			return setTimer(2000);
+		})
+		.then(data => {
+			console.log(data, positionData);
+		})
+		.catch(err => {
+			console.log(err);
+		});
+		
+	setTimer(1000).then(() => {
+		console.log('Timer done!');
+	});
+	console.log('Getting the position...');
 }
 
 button.addEventListener('click', trackUserHandler);
