e7d5f5f6baff2d87236eddd13e22b13b7750b91f
MichalWyzlic <michal.wyzlic@lennoxemea.com>
2023-03-03 21:19:05 +0100

    JS Promises done

--- JavaScript Course/Test1/Test1.js
+++ JavaScript Course/Test1/Test1.js
@@ -1,18 +1,52 @@
 'use strict'
-function productDescription(strings, name, price){
-	console.log(strings);
-	console.log(name);
-	console.log(price);
-	let priceCategory = 'well priced';
-	if(price >= 20){
-		priceCategory = 'moderately priced'
+
+function arrayToList(arr){
+	function iterate(i, arr){
+		if(i === (arr.length - 1)){
+			return new Node(arr[i], null);
+		} else {
+			return new Node(arr[i], iterate(i + 1, arr));
+		};
 	};
 
-	return `${strings[0]}${name}${strings[1]}${priceCategory}${strings[2]}`;
+	return iterate(0, arr);
 }
 
-const productName = 'Kilogram jablek';
-const cena = 65;
+function Node(value, next=null) {
+	this.value = value;
+	this.next = next;
+  }
+  
 
-const productOption = productDescription`This ${productName} is ${cena}`;
-console.log(productOption);
+function zipWith(fn,head0,head1) {
+	if(head0 === null || head1 === null){
+	  return null;
+	} else {
+		  return new Node(fn(head0.value, head1.value), zipWith(fn,head0.next,head1.next));
+	};
+};
+
+function add(a,b) {
+	return a+b;
+}
+let a = arrayToList([0,1,2,3,4,5]);
+let b = arrayToList([6,5,4,3,2,1]);
+console.log(zipWith(add, a, b));
+
+
+let boom3 = 343;
+let test = false;
+let myWireId = 0;
+
+for(let i = 0; i < 10; i++ ){
+	let text = `test = (typeof boom${i} === 'number');`;
+	console.log(eval(text));
+	if(test){
+		eval(`myWireId = boom${i};`);
+		break;
+	}
+};
+
+
+
+console.log(myWireId)
