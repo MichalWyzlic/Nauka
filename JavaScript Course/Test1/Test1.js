aba09e9c6aaf66d0f2efcefeda29c4274d3253c9
Michal Wyzlic <wyzlic.michal@gmail.com>
2023-03-07 14:26:39 +0100

    CW ipv4Parser

--- JavaScript Course/Test1/Test1.js
+++ JavaScript Course/Test1/Test1.js
@@ -1,52 +1,26 @@
 'use strict'
 
-function arrayToList(arr){
-	function iterate(i, arr){
-		if(i === (arr.length - 1)){
-			return new Node(arr[i], null);
-		} else {
-			return new Node(arr[i], iterate(i + 1, arr));
-		};
-	};
+function ipv4Parser(ip, mask){
+	//your code here
+	const ipArray = ip.split('.');
+	const maskArray = mask.split('.');
+	const result = [[0,0,0,0],[0,0,0,0]];
 
-	return iterate(0, arr);
-}
+	ipArray.forEach((element, index) => { 
+		let negMask = ~Number(mask[index]);
+		// let temp1 = Number(ipArray[index]);
+		// let temp2 = Number(maskArray[index]);
+		// let res = Number(ipArray[index]) & Number(maskArray[index]);
+		result[0][index] = Number(ipArray[index]) & Number(maskArray[index]);
+		result[1][index] = Number(ipArray[index]) & ~Number(maskArray[index]);		
+	});
+	result[0] = result[0].join('.');
+	result[1] = result[1].join('.');
 
-function Node(value, next=null) {
-	this.value = value;
-	this.next = next;
+	return result;
   }
-  
 
-function zipWith(fn,head0,head1) {
-	if(head0 === null || head1 === null){
-	  return null;
-	} else {
-		  return new Node(fn(head0.value, head1.value), zipWith(fn,head0.next,head1.next));
-	};
-};
-
-function add(a,b) {
-	return a+b;
-}
-let a = arrayToList([0,1,2,3,4,5]);
-let b = arrayToList([6,5,4,3,2,1]);
-console.log(zipWith(add, a, b));
-
-
-let boom3 = 343;
-let test = false;
-let myWireId = 0;
-
-for(let i = 0; i < 10; i++ ){
-	let text = `test = (typeof boom${i} === 'number');`;
-	console.log(eval(text));
-	if(test){
-		eval(`myWireId = boom${i};`);
-		break;
-	}
-};
-
-
-
-console.log(myWireId)
+console.log(ipv4Parser('192.168.50.1'  , '255.255.255.0'));
+console.log(ipv4Parser('192.168.50.129', '255.255.255.192'));
+console.log(ipv4Parser('192.168.50.153', '255.255.255.224'));
+console.log(ipv4Parser('65.196.188.53' , '0.0.0.0'));
