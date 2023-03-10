2bb13ea7bb400b4a5ad0b7c8d6647ee2563212e5
MichalWyzlic <michal.wyzlic@lennoxemea.com>
2023-03-10 12:41:58 +0100

    codewars

--- JavaScript Course/Test1/Test1.js
+++ JavaScript Course/Test1/Test1.js
@@ -1,26 +1,67 @@
 'use strict'
 
-function ipv4Parser(ip, mask){
-	//your code here
-	const ipArray = ip.split('.');
-	const maskArray = mask.split('.');
-	const result = [[0,0,0,0],[0,0,0,0]];
+function whiteBlackFrogs(n) {
+	if(n===1){
+		return 'wjw';
+	}
+	const refMap = new Map([
+		['WW_WB', ['j', 'WWBW_', 2]],
+		['WW_BW', ['w', 'W_WBW', -1]],
+		['WW_BB', ['w', 'W_WBB', -1]],
+		['WB_WW', ['j', '_BWWW', -2]],
+		['WB_BW', ['j', '_BWBW', -2]],
+		['WB_BB', ['j', '_BWBB', -2]],
+		['BW_WW', ['w', 'B_WWW', -1]],
+		['BW_WB', ['j', 'BWBW_', 2]],
+		['BW_BB', ['b', 'BWB_B', 1]],
+		['BB_WB', ['j', 'BBBW_', 2]],
+		['BB_BW', ['b', 'BBB_W', 1]],
+		['BB_BB', ['b', 'BBB_B', 1]],
+		['WB_', ['j', '_BW', -2]],
+		['_BW', ['b', 'B_W', 1]],
+		['B_WB', ['j', 'BBW_', 2]],
+		['BW_W', ['w', 'B_WW', -1]],
+		['WB_B', ['j', '_BWB', -2]],
+		['BW_', ['w', 'B_W', -1]],
+		['WB_W', ['j', '_BWW', -2]],
+		['W_BW', ['w', '_WBW', -1]],
+		['_WB', ['j', 'BW_', 2]],
+		['B_BW', ['b', 'BB_W', 1]],
+		['W_WB', ['j', 'WBW_', 2]],
+		['BW_B', ['b', 'BWB_', 1]],
+	]);
 
-	ipArray.forEach((element, index) => { 
-		let negMask = ~Number(mask[index]);
-		// let temp1 = Number(ipArray[index]);
-		// let temp2 = Number(maskArray[index]);
-		// let res = Number(ipArray[index]) & Number(maskArray[index]);
-		result[0][index] = Number(ipArray[index]) & Number(maskArray[index]);
-		result[1][index] = Number(ipArray[index]) & ~Number(maskArray[index]);		
-	});
-	result[0] = result[0].join('.');
-	result[1] = result[1].join('.');
+	let tempString = '';
+	for(let i = 0; i <= 2*n ; i++){
+		if(i < n){
+			tempString +='W';
+		} else if(i === n){
+			tempString += '_';
+		} else{
+			tempString += 'B';
+		};
+	};
+	let spacePosition = n;
+	let replacementString = '';
+	let mapElement = [];
+	let result = '';
+	let steps = 0;
 
-	return result;
+	while(true){
+		let start = spacePosition - 2;
+		let end = spacePosition + 3;
+		replacementString = tempString.substring(start, end);
+		mapElement = refMap.get(replacementString);
+		if(mapElement === undefined){
+			break;
+		}
+		tempString = tempString.substring(0, start) + mapElement[1] + tempString.substring(end);
+		result += mapElement[0];
+		spacePosition += mapElement[2];
+		steps ++;
+		}
+	return result + ' ' + steps;
   }
 
-console.log(ipv4Parser('192.168.50.1'  , '255.255.255.0'));
-console.log(ipv4Parser('192.168.50.129', '255.255.255.192'));
-console.log(ipv4Parser('192.168.50.153', '255.255.255.224'));
-console.log(ipv4Parser('65.196.188.53' , '0.0.0.0'));
+console.log(whiteBlackFrogs(2));
+
