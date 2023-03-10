80e3c6befe70a02a6b422a2e5fad289ee5beda58
MichalWyzlic <michal.wyzlic@lennoxemea.com>
2023-03-10 14:09:55 +0100

    JS 372 done

--- JavaScript Course/http-01-starting-setup/assets/scripts/app.js
+++ JavaScript Course/http-01-starting-setup/assets/scripts/app.js
@@ -1,25 +1,41 @@
-const xhr = new XMLHttpRequest();
-
 const listElement = document.querySelector('.posts');
 const templateListItem = document.getElementById('single-post');
 
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
-xhr.responseType = 'json';
+function sendHTTPRequest(method, url){
+	const promise = new Promise((resolve, reject) =>{
+			const xhr = new XMLHttpRequest();
+		xhr.open(method, url);
+		xhr.responseType = 'json';
 
-xhr.addEventListener('load', function(){
-	const listOfPosts = xhr.response;
-	console.log(listOfPosts);
-	for(const post of listOfPosts){
-		const listItem = document.importNode(templateListItem.content, true);
-		listItem.querySelector('h2').textContent = post.title;
-		listItem.querySelector('p').textContent = post.body;
-		listElement.append(listItem);
+		xhr.addEventListener('load', function(){
+			resolve(xhr.response);
+			
+		});
+		xhr.send();
+	});
 
-	}
-});
+	return promise;	
+};
 
-xhr.send();
+function fetchPosts(){
+	sendHTTPRequest('GET', 'https://jsonplaceholder.typicode.com/posts').then(
+		responseData => {
+			const listOfPosts = responseData// xhr.response;
+			console.log(listOfPosts);
+			for(const post of listOfPosts){
+				const listItem = document.importNode(templateListItem.content, true);
+				listItem.querySelector('h2').textContent = post.title;
+				listItem.querySelector('p').textContent = post.body;
+				listElement.append(listItem);
 
+			};
+	});
+
+};
+
+fetchPosts();
+
+// 
 // const listElement = document.querySelector('.posts');
 // const templateListItem = document.getElementById('single-post');
 
