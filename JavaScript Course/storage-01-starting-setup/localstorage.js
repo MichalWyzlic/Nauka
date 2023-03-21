478bba52bf35942cf3d84a7115a7dd4d1df328cc
Michal Wyzlic <wyzlic.michal@gmail.com>
2023-03-21 14:58:08 +0100

    JS 429 done

---
+++ JavaScript Course/storage-01-starting-setup/localstorage.js
@@ -0,0 +1,30 @@
+const storeBtn = document.getElementById('store-btn');
+const retrBtn = document.getElementById('retrieve-btn');
+
+const userId = 'u123';
+const user = {
+	name: 'Max',
+	age: 30,
+	hobbies: ['Sports', 'Cooking']
+}
+
+storeBtn.addEventListener('click', () => {
+	localStorage.setItem('uid', userId);
+	localStorage.setItem('user', JSON.stringify(user));
+});
+
+retrBtn.addEventListener('click', () => {
+	const extractedID = localStorage.getItem('uid');
+	const extractedUser = JSON.parse(localStorage.getItem('user'));
+	if(extractedUser){
+		console.log('Got the user.');
+		console.log(extractedUser);
+	} else {
+		console.log('Could not find the USER.');
+	};
+	if(extractedID){
+		console.log('Got the ID - ' + extractedID);
+	} else {
+		console.log('Could not find ID.');
+	};
+});
