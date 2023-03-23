f68d63c2677a7fa0cf785360f9e8c4a00037b2e9
Michal Wyzlic <wyzlic.michal@gmail.com>
2023-03-23 17:21:06 +0100

    JS 431 done

---
+++ JavaScript Course/storage-01-starting-setup/cookies.js
@@ -0,0 +1,25 @@
+console.log(document.cookie);
+
+const storeBtn = document.getElementById('store-btn');
+const retrBtn = document.getElementById('retrieve-btn');
+
+const userId = 'u123';
+const user = {
+	name: 'Max',
+	age: 30,
+	hobbies: ['Sports', 'Cooking']
+}
+
+storeBtn.addEventListener('click', () => {
+	document.cookie = `uid=${userId}; max-age=360`;
+	document.cookie = `user=${JSON.stringify(user)}`;
+});
+
+retrBtn.addEventListener('click', () => {
+	const data = document.cookie.split(';').map((item) => {
+		return item.trim();
+	});
+	console.log(data);
+	console.log(JSON.parse(data[1].split('=')[1]));
+	
+});
