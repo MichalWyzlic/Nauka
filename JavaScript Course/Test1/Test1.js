2dbbfbc93bdadf43c1efa564c8aca4df79cd9ad2
Michal Wyzlic <wyzlic.michal@gmail.com>
2023-03-29 13:34:39 +0200

    codewars kata

--- JavaScript Course/Test1/Test1.js
+++ JavaScript Course/Test1/Test1.js
@@ -24,30 +24,26 @@
 
 // console.log(permutations(230, 3,[91, 74, 73, 85, 73, 81, 87]));
 
-class Ship{
-	constructor(direction, size, up, left){
-		this.direction = direction;
-		this.size = size;
-		this.up = up;
-		this.left = left;
-	};
-}
-
 function validateBattlefield(field) {
 	// write your magic here
-	const myBattleField = new Array(10).fill(new Array(10).fill(0));
+	const myBattleField = [
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	];
 	let ships = new Array(4).fill(0);
-	let previous = 0;
 
 	function checkHorizontal(i,j){
-		let count = 1;
-		// myBattleField[i][j] = 1;
-		// if (i < 9){
-		// 	myBattleField[i+1][j] = 'x';
-		// };
-		// j++;
+		let count = 0;
 		while(j < 10 && count < 5){
-			if (myBattleField[i][j] !== 0){
+			if (myBattleField[i][j] !== 0 && !(myBattleField[i][j] === 'x' && field[i][j] === 0)){
 				return -1;
 			};
 			if (field[i][j] === 1) {
@@ -55,6 +51,9 @@ function validateBattlefield(field) {
 				myBattleField[i][j] = `H${count}`;
 				if (i < 9){
 					myBattleField[i+1][j] = 'x';
+					if (j > 0 && count ===1){
+						myBattleField[i+1][j-1] = 'x';
+					};
 				};
 			} else {
 				myBattleField[i][j] = `x`;
@@ -64,20 +63,14 @@ function validateBattlefield(field) {
 				break;
 			};
 			j++;
-		};
-		
+		};		
 		return (count < 5) ? count : -1;
 	};
 
 	function checkVertical(i,j){
-		let count = 1;
-		// myBattleField[i][j] = 1;
-		// if (i < 9){
-		// 	myBattleField[i+1][j] = 'x';
-		// };
-		// j++;
+		let count = 0;
 		while(i < 10 && count < 5){
-			if (myBattleField[i][j] !== 0){
+			if (myBattleField[i][j] !== 0 && !(myBattleField[i][j] === 'x' && field[i][j] === 0)){
 				return -1;
 			};
 			if (field[i][j] === 1) {
@@ -91,27 +84,37 @@ function validateBattlefield(field) {
 				if (j < 9){
 					myBattleField[i][j+1] = 'x';
 				};
+				if (j > 0){
+					myBattleField[i][j-1] = 'x';
+				};
 				break;
 			};
 			i++;
-		};
-		
+		};		
 		return (count < 5) ? count : -1;
 	};
 
 	let result = 0;
-
 	for(let i = 0; i < 10; i++){
 		for(let j = 0; j < 10; j++){
 			if (myBattleField[i][j] === 0){ 
 				if (field[i][j] === 1){
-					myBattleField[i][j] = 1;
+					//myBattleField[i][j] = 1;
 					if(i < 10 && (j === 9 || field[i][j+1] === 0)){
 						result = checkVertical(i,j);
 					} else if((i === 9 || field[i+1][j] === 0) && j < 10){
 						result = checkHorizontal(i,j);
 					} else {
-						ships[0] += 'S1';
+						myBattleField[i][j] = 'S1';
+						if (j < 9){
+							myBattleField[i][j+1] = 'x';
+						};
+						if (i < 9){
+							myBattleField[i+1][j] = 'x';
+						};
+						if (i < 9 && j < 9){
+							myBattleField[i+1][j+1] = 'x';
+						};
 						result = 1;
 					};
 					if (result === -1){
@@ -119,17 +122,12 @@ function validateBattlefield(field) {
 					};
 					ships[result - 1] += 1;
 				};
-			} else if (field[i][j] === 1){
+			} else if (field[i][j] === 1 && myBattleField[i][j] === 'x'){
 				return false;
-			}	
-			
-			
-
-			
-
-		}
-	}
-
+			};	
+		};
+	};
+	return (ships[0] === 4 && ships[1] === 3 && ships[2] === 2 && ships[3] === 1);
   };
 
   console.log(validateBattlefield([
