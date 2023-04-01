1794a7a091c44b1802bb457f96c9b6180dc64e3d
Michal Wyzlic <wyzlic.michal@gmail.com>
2023-04-01 15:15:33 +0200

    JS 486 done

--- JavaScript Course/meta-01-starting-setup/app.js
+++ JavaScript Course/meta-01-starting-setup/app.js
@@ -8,4 +8,73 @@ const person = {
 	[Symbol.toStringTag]: 'User'
 };
 
-person.uid = 'm';
+const company = {
+	curEmployee: 0,
+	employees: ['Herve','Ania','James'],
+	// next() {
+	// 	if(this.curEmployee >= this.employees.length){
+	// 		return { value: this.curEmployee, done: true};
+	// 	};
+	// 	const returnValue = {
+	// 		value: this.employees[this.curEmployee],
+	// 		done: false
+	// 	};
+	// 	this.curEmployee ++;
+	// 	return returnValue;
+	// },
+	[Symbol.iterator]: function* employeeGenerator(){
+		// let employee = company.next();
+
+		// while(!employee.done){
+		// 	yield employee.value;
+		// 	employee = company.next();
+		// };
+
+		let currentEmployee = 0;
+		while(currentEmployee < this.employees.length){
+			yield this.employees[currentEmployee];
+			currentEmployee ++;
+		}
+	}
+};
+
+// let employee = company.next();
+
+// while(!employee.done){
+// 	console.log(employee.value);
+// 	employee = company.next();
+// };
+
+// const it = company.getEmployee();
+
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+
+for(const employee of company){
+	console.log(employee);
+};
+
+console.log([...company]);
+
+//------
+
+const course = {
+	title: 'JavaScript - The Complete Guide'
+};
+
+Reflect.setPrototypeOf(course, {toString() {return this.title}});
+console.log(course.toString());
+
+const courseHandler = {
+	get(obj, propertyName) {
+		console.log(propertyName);
+		return obj[propertyName]; 
+
+	}
+};
+
+const pCourse = new Proxy(course, courseHandler);
+console.log(pCourse.title);
