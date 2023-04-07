2bfa40d5a7fd9c20c1ac9aff2a2f9ee4abaad82b
Michal Wyzlic <wyzlic.michal@gmail.com>
2023-04-07 22:42:42 +0200

    kata ractangels

--- JavaScript Course/Test1/Test1.js
+++ JavaScript Course/Test1/Test1.js
@@ -1,56 +1,3 @@
-// const rectArray = [
-// 	[3, 3, 8, 5],
-// 	[6, 3, 8, 9],
-// 	[11, 6, 14, 12]
-// ];
-// const x1 = 0;
-// const x2 = 2;
-// const y1 = 1;
-// const y2 = 3;
-
-// function testRectangles(rectArray) {
-// 	function rectArea(rect1) {
-// 		return (rect1[x2] - rect1[x1]) * (rect1[y2] - rect1[y1]);
-// 	}
-
-// 	function commonArea(rect1, rect2) {
-// 		if (
-// 			((rect2[x1] > rect1[x1] && rect2[x1] < rect1[x2]) ||
-// 				(rect2[y2] > rect1[y1] && rect2[y2] < rect1[y2]) ||
-// 				(rect2[y1] <= rect1[y1] && rect2[y2] >= rect1[y2])) &&
-// 			((rect2[y1] > rect1[y1] && rect2[y1] < rect1[y2]) ||
-// 				(rect2[y2] > rect1[y1] && rect2[y2] < rect1[y2]) ||
-// 				(rect2[y1] <= rect1[y1] && rect2[y2] >= rect1[y2]))
-// 		) {
-// 			const newX1 = Math.max(rect1[x1], rect2[x1]);
-// 			const newX2 = Math.min(rect1[x2], rect2[x2]);
-// 			const newY1 = Math.max(rect1[y1], rect2[y1]);
-// 			const newY2 = Math.min(rect1[y2], rect2[y2]);
-// 			return (newX2 - newX1) * (newY2 - newY1);
-// 		}
-// 	}
-// }
-
-// function commonArea(rect1, rect2) {
-// 	if (
-// 		((rect2[x1] > rect1[x1] && rect2[x1] < rect1[x2]) ||
-// 			(rect2[y2] > rect1[y1] && rect2[y2] < rect1[y2]) ||
-// 			(rect2[y1] <= rect1[y1] && rect2[y2] >= rect1[y2])) &&
-// 		((rect2[y1] > rect1[y1] && rect2[y1] < rect1[y2]) ||
-// 			(rect2[y2] > rect1[y1] && rect2[y2] < rect1[y2]) ||
-// 			(rect2[y1] <= rect1[y1] && rect2[y2] >= rect1[y2]))
-// 	) {
-// 		const newX1 = Math.max(rect1[x1], rect2[x1]);
-// 		const newX2 = Math.min(rect1[x2], rect2[x2]);
-// 		const newY1 = Math.max(rect1[y1], rect2[y1]);
-// 		const newY2 = Math.min(rect1[y2], rect2[y2]);
-// 		return (newX2 - newX1) * (newY2 - newY1);
-// 	}
-// 	return 0;
-// }
-
-// console.log(commonArea([1, 4, 8, 6], [2, 1, 5, 3]));
-
 class Node {
 	value = undefined;
 	previous = null;
@@ -64,29 +11,47 @@ class OrderedList {
 	//Value needs to be an array
 	//[xCoord, [y1Coord, y2Coord, start/end], [y1Coord, y2Coord, start/end] ...]
 	header = null;
-	constructor(initValue) {
-		if (initValue) {
-			this.header = new Node(initValue);
+	constructor(rectArr) {
+		if (rectArr) {
+			this.header = new Node([
+				rectArr[0],
+				[rectArr[1], rectArr[3], true]
+			]);
+			this.header.next = new Node([
+				rectArr[2],
+				[rectArr[1], rectArr[3], false]
+			]);
+			this.header.next.previous = this.header;
 		}
 	}
 
 	putBefore(newNode, node) {
 		newNode.previous = node.previous;
 		newNode.next = node;
+		if (node.previous) {
+			node.previous.next = newNode;
+		}
 		node.previous = newNode;
-		return node;
+		return newNode;
 	}
 
 	putAfter(newNode, node) {
 		newNode.next = node.next;
 		newNode.previous = node;
+		if (node.next) {
+			node.next.previous = newNode;
+		}
 		node.next = newNode;
-		return node;
+		return newNode;
 	}
 
 	add(node) {
 		let currentNode = this.header;
 		//Replace header
+		if (this.header === null) {
+			this.header = node;
+			return this.header;
+		}
 		if (node.value[0] < currentNode.value[0]) {
 			this.header.previous = node;
 			node.next = this.header;
@@ -97,14 +62,14 @@ class OrderedList {
 		while (currentNode) {
 			if (node.value[0] < currentNode.value[0]) {
 				if (currentNode.previous === null) {
-					return this.putBefore(node, currentNode);	
+					return this.putBefore(node, currentNode);
 				} else if (node.value[0] > currentNode.previous.value[0]) {
 					return this.putBefore(node, currentNode);
 				} else if (node.value[0] < currentNode.previous.value[0]) {
 					currentNode = currentNode.previous;
 				} else if (node.value[0] === currentNode.previous.value[0]) {
-					for (let i = 1; i < node.length; i++) {
-						currentNode.previous.value.push(node[i]);
+					for (let i = 1; i < node.value.length; i++) {
+						currentNode.previous.value.push(node.value[i]);
 					}
 					return currentNode.previous;
 				}
@@ -116,14 +81,14 @@ class OrderedList {
 				} else if (node.value[0] > currentNode.next.value[0]) {
 					currentNode = currentNode.next;
 				} else if (node.value[0] === currentNode.next.value[0]) {
-					for (let i = 1; i < node.length; i++) {
-						currentNode.next.value.push(node[i]);
+					for (let i = 1; i < node.value.length; i++) {
+						currentNode.next.value.push(node.value[i]);
 					}
 					return currentNode.next;
 				}
 			} else {
-				for (let i = 1; i < node.length; i++) {
-					currentNode.value.push(node[i]);
+				for (let i = 1; i < node.value.length; i++) {
+					currentNode.value.push(node.value[i]);
 				}
 				return currentNode;
 			}
@@ -131,55 +96,235 @@ class OrderedList {
 		return node;
 	}
 
-	addEndNode(startNode, endNode){
-		//const i = startNode.value.length -1;
-		const startNodeValue = startNode.value;
-		let currentNode = startNode;
-				
-		while (true) {
+	addStartNode(startNode) {
+		let currentNode = this.header;
+		let startNodeValue = startNode.value[1];
+		//Replace header
+		if (this.header === null) {
+			this.header = startNode;
+			return this.header;
+		}
+		if (startNode.value[0] < currentNode.value[0]) {
+			this.header.previous = startNode;
+			startNode.next = this.header;
+			this.header = startNode;
+			return this.header;
+		}
 
-
-
-
-			//Check the code below
-			if (!currentNode.next) {
-				currentNode.next = endNode;
-			} else if (endNode.value[0] < currentNode.next.value[0]) {
-				endNode.next = currentNode.next;
-				currentNode.next.previous = endNode;
-				endNode.previous = currentNode;				
-				return endNode;
-			} else if (endNode.value[0] === currentNode.next.value[0]) {
-				for (let i = 1; i < endNode.length; i++) {
-					currentNode.next.value.push(node[i]);
+		while (currentNode) {
+			if (startNode.value[0] < currentNode.value[0]) {
+				if (currentNode.previous === null) {
+					return this.putBefore(startNode, currentNode);
+				} else if (startNode.value[0] > currentNode.previous.value[0]) {
+					//copy start values of the previous point
+					for (
+						let i = 1;
+						i < currentNode.previous.value.length;
+						i++
+					) {
+						if (currentNode.previous.value[i][2]) {
+							startNode.value.push(currentNode.previous.value[i]);
+						}
+					}
+					return this.putBefore(startNode, currentNode);
+				} else if (startNode.value[0] < currentNode.previous.value[0]) {
+					currentNode = currentNode.previous;
+				} else if (
+					startNode.value[0] === currentNode.previous.value[0]
+				) {
+					for (let i = 1; i < startNode.value.length; i++) {
+						currentNode.previous.value.push(startNode.value[i]);
+					}
+					return currentNode.previous;
 				}
-				return currentNode.next;
+			} else if (startNode.value[0] > currentNode.value[0]) {
+				if (currentNode.next === null) {
+					//copy start values of the current node
+					for (let i = 1; i < currentNode.value.length; i++) {
+						if (currentNode.value[i][2]) {
+							startNode.value.push(currentNode.value[i]);
+						}
+					}
+					return this.putAfter(startNode, currentNode);
+				} else if (startNode.value[0] < currentNode.next.value[0]) {
+					//copy start values of the current node
+					for (let i = 1; i < currentNode.value.length; i++) {
+						if (currentNode.value[i][2]) {
+							startNode.value.push(currentNode.value[i]);
+						}
+					}
+					return this.putAfter(startNode, currentNode);
+				} else if (startNode.value[0] > currentNode.next.value[0]) {
+					currentNode = currentNode.next;
+				} else if (startNode.value[0] === currentNode.next.value[0]) {
+					for (let i = 1; i < startNode.value.length; i++) {
+						currentNode.next.value.push(startNode.value[i]);
+					}
+					return currentNode.next;
+				}
 			} else {
-				for (let i = 1; i < endNode.length; i++) {
-					currentNode.value.push(node[i]);
+				for (let i = 1; i < startNode.value.length; i++) {
+					currentNode.value.push(startNode.value[i]);
 				}
 				return currentNode;
+			}
+		}
+		return startNode;
+	}
+
+	addEndNode(startNode, endNode) {
+		//const i = startNode.value.length -1;
+		const startNodeValue = [endNode.value[1][0], endNode.value[1][1], true];
+		let currentNode = startNode;
+
+		while (true) {
+			if (!currentNode.next) {
+				currentNode.next = endNode;
+				endNode.previous = currentNode;
+				let startRemoved = false;
+				for (let i = 1; i < currentNode.value.length; i++) {
+					if (currentNode.value[i][2]) {
+						if (
+							currentNode.value[i][0] === endNode.value[1][0] &&
+							currentNode.value[i][1] === endNode.value[1][1] &&
+							!startRemoved
+						) {
+							startRemoved = true;
+						} else {
+							endNode.value.push(currentNode.value[i]);
+						}
+					}
+				}
+				return endNode;
+			} else if (endNode.value[0] < currentNode.next.value[0]) {
+				endNode.next = currentNode.next;
+				currentNode.next = endNode;
+				currentNode.next.previous = endNode;
+				endNode.previous = currentNode;
+				let startRemoved = false;
+				for (let i = 1; i < currentNode.value.length; i++) {
+					if (currentNode.value[i][2]) {
+						if (
+							currentNode.value[i][0] === endNode.value[1][0] &&
+							currentNode.value[i][1] === endNode.value[1][1] &&
+							!startRemoved
+						) {
+							startRemoved = true;
+						} else {
+							endNode.value.push(currentNode.value[i]);
+						}
+					}
+				}
+				return endNode;
+			} else if (endNode.value[0] === currentNode.next.value[0]) {
+				currentNode.next.value.push(endNode.value[1]);
+				return currentNode.next;
+			} else {
+				currentNode.next.value.push(startNodeValue);
+				currentNode = currentNode.next;
 			}
 		}
 		return endNode;
 	}
 
-
-
-	addRectangle(startNode, endNode){
-		const tempStatNode = this.add(startNode);
-
+	addRectangle(rectArr) {
+		//rectArr format [x1,y1,x2,y2]
+		const initialStartNodeValue = [rectArr[1]];
+		const tempStatNode = this.addStartNode(
+			new Node([rectArr[0], [rectArr[1], rectArr[3], true]])
+		);
+		this.addEndNode(
+			tempStatNode,
+			new Node([rectArr[2], [rectArr[1], rectArr[3], false]])
+		);
 	}
 }
 
-const myList = new OrderedList([2, [0, 4, true]]);
-myList.add(new Node([4, [0, 4, false]]));
+function calculate(rectArray) {
+	console.log(rectArray);
+	if (rectArray.length === 0) {
+		return 0;
+	}
+	const myList = new OrderedList(rectArray[0]);
+	for (let i = 1; i < rectArray.length; i++) {
+		myList.addRectangle(rectArray[i]);
+	}
 
-myList.add(new Node([1, [1, 2, true]]));
-myList.add(new Node([7, [1, 2, false]]));
+	let currentNode = myList.header;
+	let sum = 0;
 
-myList.add(new Node([3, [3, 7, true]]));
-myList.add(new Node([5, [3, 7, false]]));
+	while (currentNode.next) {
+		let sum1 = 0;
+		const x1 = currentNode.value[0];
+		const x2 = currentNode.next.value[0];
+		const myTestPoint = new Array();
+		for (let i = 1; i < currentNode.value.length; i++) {
+			myTestPoint.push(currentNode.value[i].slice());
+		}
 
-myList.add(new Node([6, [3, 5, true]]));
-myList.add(new Node([7, [3, 5, false]]));
+		while (!myTestPoint[0][2]) {
+			myTestPoint.shift();
+			if (myTestPoint.length === 0) {
+				break;
+			}
+		}
+
+		myTestPoint.sort((a, b) => a[0] - b[0]);
+		if (myTestPoint[0]) {
+			while (!myTestPoint[0][2]) {
+				myTestPoint.shift();
+				if (myTestPoint.length === 0) {
+					break;
+				}
+			}
+		}
+		let i = 0;
+		if (myTestPoint.length === 1) {
+			sum1 = myTestPoint[i][1] - myTestPoint[i][0];
+		} else {
+			while (i < myTestPoint.length) {
+				if (i >= myTestPoint.length - 1) {
+					sum1 += myTestPoint[i][1] - myTestPoint[i][0];
+					break;
+				} else if (!myTestPoint[i + 1][2]) {
+					myTestPoint.splice(i + 1, 1);
+				} else if (myTestPoint[i + 1][0] <= myTestPoint[i][1]) {
+					myTestPoint[i][1] = Math.max(
+						myTestPoint[i][1],
+						myTestPoint[i + 1][1]
+					);
+					myTestPoint.splice(i + 1, 1);
+				} else {
+					sum1 += myTestPoint[i][1] - myTestPoint[i][0];
+					i++;
+				}
+			}
+		}
+
+		sum += sum1 * (x2 - x1);
+		currentNode = currentNode.next;
+	}
+	return sum;
+}
+
+const rectArray1 = [
+	[1, 1, 2, 2],
+	[2, 2, 3, 3],
+	[3, 3, 4, 4],
+	[4, 4, 5, 5],
+	[2, 1, 3, 2]
+];
+
+// const rectArray = [
+// 	[2, 0, 4, 4],
+// 	[1, 1, 7, 2],
+// 	[3, 3, 5, 7],
+// 	[6, 3, 7, 5],
+// 	[5, 1, 9, 2]
+// ];
+
+// if(rectArray.length === 0){
+//     return 0;
+// }
+
+console.log(calculate(rectArray1));
--- JavaScript Course/practice-places-01-starting-setup/src/SharePlace.js
+++ JavaScript Course/practice-places-01-starting-setup/src/SharePlace.js
@@ -2,100 +2,146 @@ import { Modal } from './UI/Modal';
 import { Map } from './UI/Map';
 import { getCoordsFromAddress, getAddressFromCoords } from './Utility/Location';
 
-class PlaceFinder{
-	constructor(){
+class PlaceFinder {
+	constructor() {
 		const addressForm = document.querySelector('form');
 		const locateUserBtn = document.getElementById('locate-btn');
 		this.shareBtn = document.getElementById('share-btn');
 
-		locateUserBtn.addEventListener('click', this.locateUserHandler.bind(this));
-		this.shareBtn.addEventListener('click', this.sharePlaceHandler.bind(this));
-		addressForm.addEventListener('submit', this.findAddressHandler.bind(this));
-	};
+		locateUserBtn.addEventListener(
+			'click',
+			this.locateUserHandler.bind(this)
+		);
+		this.shareBtn.addEventListener(
+			'click',
+			this.sharePlaceHandler.bind(this)
+		);
+		addressForm.addEventListener(
+			'submit',
+			this.findAddressHandler.bind(this)
+		);
+	}
 
-	sharePlaceHandler(){
+	sharePlaceHandler() {
 		const shareLinkInputElement = document.getElementById('share-link');
-		if(!navigator.clipboard){
+		if (!navigator.clipboard) {
 			shareLinkInputElement.select();
 			return;
-		};
+		}
 
-		navigator.clipboard.writeText(shareLinkInputElement.value)
-			.then(response => {
-				alert('Copied into clipboard.')
-			}).catch( err => {
+		navigator.clipboard
+			.writeText(shareLinkInputElement.value)
+			.then((response) => {
+				alert('Copied into clipboard.');
+			})
+			.catch((err) => {
 				console.log(err);
 				shareLinkInputElement.select();
 			});
+	}
 
-	};
-
-	selectPlace(coordinates, address){
-		if(this.map){
+	selectPlace(coordinates, address) {
+		if (this.map) {
 			this.map.render(coordinates);
 		} else {
 			this.map = new Map(coordinates);
-		};
+		}
+		fetch('http://localhost:3000/add-location', {
+			method: 'POST',
+			body: JSON.stringify({
+				address: address,
+				lat: coordinates.lat,
+				lng: coordinates.lng
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				console.log(data);
+			});
 		this.shareBtn.disabled = false;
 		const shareLinkInputElement = document.getElementById('share-link');
-		shareLinkInputElement.value = `${location.origin}/my-place?address=${encodeURI(address)}&lat=${coordinates.lat}&&lng=${coordinates.lng}`;
-		
-	};
+		shareLinkInputElement.value = `${
+			location.origin
+		}/my-place?address=${encodeURI(address)}&lat=${coordinates.lat}&&lng=${
+			coordinates.lng
+		}`;
+	}
 
-	locateUserHandler(){
-		if (!navigator.geolocation){
-			alert('Geolocation is not available in your browser. \nPlease enter the address manually');
+	locateUserHandler() {
+		if (!navigator.geolocation) {
+			alert(
+				'Geolocation is not available in your browser. \nPlease enter the address manually'
+			);
 			return;
-		};
-		const modal = new Modal('loading-modal-content', 'Loading location, please wait.');
+		}
+		const modal = new Modal(
+			'loading-modal-content',
+			'Loading location, please wait.'
+		);
 		modal.show();
-		navigator.geolocation.getCurrentPosition(successResult => {
-			const coordinates = {
-				lat: successResult.coords.latitude,
-				lng: successResult.coords.longitude
-			};
-			console.log(coordinates);
-			
-			getAddressFromCoords(coordinates).then(result => {
-				const address = result;
-				console.log(address);
-				this.selectPlace(coordinates, address);
-				modal.hide();	
-			}, error => {
-				alert('Could not get the address form given position.');
-				modal.hide();
-			});
-			
-		}, error => {
-			alert('Could not locate you, please enter the address manually!');
-			modal.hide();
-		});
-	};
+		navigator.geolocation.getCurrentPosition(
+			(successResult) => {
+				const coordinates = {
+					lat: successResult.coords.latitude,
+					lng: successResult.coords.longitude
+				};
+				console.log(coordinates);
 
-	findAddressHandler(event){
+				getAddressFromCoords(coordinates).then(
+					(result) => {
+						const address = result;
+						console.log(address);
+						this.selectPlace(coordinates, address);
+						modal.hide();
+					},
+					(error) => {
+						alert('Could not get the address form given position.');
+						modal.hide();
+					}
+				);
+			},
+			(error) => {
+				alert(
+					'Could not locate you, please enter the address manually!'
+				);
+				modal.hide();
+			}
+		);
+	}
+
+	findAddressHandler(event) {
 		event.preventDefault();
 		const address = event.target.querySelector('input').value;
-		if(!address || address.trim().length ===0){
-			alert('Invalid address. Enter a valid one.')
+		if (!address || address.trim().length === 0) {
+			alert('Invalid address. Enter a valid one.');
 			return;
-		};
+		}
 
-		const modal = new Modal('loading-modal-content', 'Loading location, please wait.');
+		const modal = new Modal(
+			'loading-modal-content',
+			'Loading location, please wait.'
+		);
 		modal.show();
-		
-		getCoordsFromAddress(address).then(result => {
-			//console.log('findAddessHandler result '+result);
-			const coordinates = result;
-			//console.log(coordinates);
-			modal.hide();
-			this.selectPlace(coordinates, address);
 
-		}, error => {
-			alert('Could not get the position form given address.');
-			modal.hide();
-		});
-
-	};
-};
+		getCoordsFromAddress(address).then(
+			(result) => {
+				//console.log('findAddessHandler result '+result);
+				const coordinates = result;
+				//console.log(coordinates);
+				modal.hide();
+				this.selectPlace(coordinates, address);
+			},
+			(error) => {
+				alert('Could not get the position form given address.');
+				modal.hide();
+			}
+		);
+	}
+}
 
 new PlaceFinder();
