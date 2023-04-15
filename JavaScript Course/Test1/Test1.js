8f6fbf30caaa0fda35cacd6347bc0c237d2aab63
Michal Wyzlic <wyzlic.michal@gmail.com>
2023-04-15 12:21:18 +0200

    Codewars

--- JavaScript Course/Test1/Test1.js
+++ JavaScript Course/Test1/Test1.js
@@ -1,122 +1,184 @@
-const test1 = [
-	-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18,
-	19, 20
-];
+const testMatrix = [2,7,1,10,9,8,6,4,5,3];
 
-const test2 = [
-	-87,-85,-82,-79,-76,-75,-72,-70,-67,-65,-63,-62,-60,-59,-58,-57,-55
-
-]
-
-// function solution(list) {
-// 	if(list.length === 0){
-// 		return '';
-// 	} else if (list.length === 1){
-// 		return `${list[0]}`;
-// 	}
-// 	let result = ``;
-// 	let prev = list[0];
-// 	let sequence = false;
-	
-// 	let i = 1;
-// 	while(i < list.length) {
-// 		let next = list[i];
-// 		if(i < list.length)
-// 		if ((prev + 1) === next){
-// 			if (!sequence){
-// 				result += `${prev}-`;
+// function smaller(arr){
+// 	const result = new Array(arr.length).fill(0);
+// 	for(let i = 0; i < arr.length; i++){
+// 		for(let j = i+1; j < arr.length; j++){
+// 			if(arr[i] > arr[j]){
+// 				result[i] ++;
 // 			}
-// 			sequence = true;
-// 		} else {
-// 			result += `${prev},`;
-// 			sequence = false;
 // 		}
-// 		if(i >= list.length-1){
-// 			result += `${next}`;
-// 			break;
-// 		}
-// 		prev = list[i];
-// 		i++;
-// 	}	
+// 	}
 // 	return result;
 // }
 
-function solution(list) {
-	let result = ``;
-	if(list.length <= 2){
-		result = list.join(',');
-		return result;
-	};
 
-	
-	let start;
-	let next1;
-	let next2;
-	let i = 0;
-	
-	while(i<=list.length-3){
-		start = list[i];
-		next1 = list[i+1];
-		next2 = list[i+2];
-		if(start + 1 === next1){
-			if (next1 + 1 === next2){
-				result += `${start}-`;
-				i += 2;
-				if(i >= list.length - 1){
-					result += `${next2}`;
-					i++;
+function smaller(arr){
+	let min = arr[arr.length -1];
+	let max = arr[arr.length -1];
+	const result = [];//new Array(arr.length).fill(0);
+	result[arr.length -1] = 0
+	const sortedArray =[arr[arr.length-1]];
+	for(let i = arr.length-2; i >= 0; i--){
+		if(arr[i] <= min){
+			min = arr[i];
+			result[i] = 0;
+			sortedArray.unshift(min);
+		} else if(arr[i] > max){
+			max = arr[i];
+			result[i] = sortedArray.length;
+			sortedArray.push(max);
+		} else{
+			for(let j = 0; j <= sortedArray.length; j++){
+				if(j === sortedArray.length){
+					sortedArray.push(arr[i]);
+					result[i] = j;
 					break;
-				};
-				while(true){
-					next1 = list[i];
-					next2 = list[i+1];
-					if(next1 + 1 !== next2){
-						result += `${next1},`;
-						i++;
-						break;
-					} else {
-						i ++;
-						if(i >= list.length -1){
-							result += `${next2}`;
-							i ++;
-							break;
-						};
-					};
-				};
-			} else {
-				result += `${start},`;
-				i += 1;
-				if(i >= list.length -2){
-					result += `${next1},${next2}`;
-					i += 2;
+				}
+				if(arr[i] <= sortedArray[j]){
+					sortedArray.splice(j,0,arr[i]);
+					result[i] = j;
 					break;
-				};
+				}
 			}
-		} else {
-			result += `${start},`;
-				i ++;
-				if(i >= list.length -2){
-					result += `${next1},${next2}`;
-					i += 2;
-					break;
-				}; 
-		};
-
-
-	};
-
-	while(i <= list.length -1){
-		if(i = list.length - 1){
-			result += `${list[i]}`;
-		} else {
-			result += `${list[i]},`;
-		};
-		i++;
-	};	
-	
+		}
+	}
 	return result;
+}
+
+
+function mergeSort(arr){
+	let result = [];
+	let tempArr1 = [];
+	let tempArr2 = [];
+
+	//first pass
+	let i = 0;
+	for(let i = 0; i < arr.length/2; i++){
+			let pos = i*2;
+			if(arr[pos] > arr[pos+1]){
+				tempArr1[i]=[];
+				tempArr1[i][0]=[arr[pos+1],pos+1,0];
+				result[pos+1] = 0;
+				tempArr1[i][1]=[arr[pos],pos,1];					
+				result[pos] = 1;
+			} else {
+				tempArr1[i]=[];
+				tempArr1[i][0]=[arr[pos],pos,0];
+				result[pos] = 0;
+				tempArr1[i][1]=[arr[pos+1],pos+1,0];
+				result[pos+1] = 0;
+			}
+	}
+	let reverse = (arr.length % 2);
+	if(reverse){
+		tempArr1.push([arr[arr.length-1],arr.length-1,0]);
+	}
+	while(tempArr1.length > 1){
+		tempArr2 = [];
+		let iterator = 0;
+		let posA = 0;
+		let posB = 1;
+		if(reverse){
+			posB = tempArr1.length -1;
+			posA = posB - 1;
+			iterator = (Math.ceil(tempArr1.length/2) - 1);
+		};
+
+		// while( reverse ? posB > 0 : posA < tempArr1.length - 1){
+		while( tempArr1.length > 1){	
+			let lenPosA = tempArr1[posA].length;
+			let lenPosB = tempArr1[posB].length;
+			let increase = 0;
+			tempArr2[iterator]=[];
+			while((tempArr1[posA].length > 0) && (tempArr1[posB].length > 0)){
+				if(tempArr1[posA][0]>tempArr1[posB][0]){
+					tempArr2[iterator].push(tempArr1[posB].shift());
+					increase = tempArr2[iterator].length - (lenPosA + (lenPosB - tempArr1[posB].length));
+					if(increase >0){
+						let originalPosition = tempArr2[iterator].at(-1)[1];
+						result[originalPosition] += increase;
+					}
+					
+				} else {
+					tempArr2[iterator].push(tempArr1[posA].shift());
+					increase = tempArr2[iterator].length - (lenPosA - tempArr1[posA].length);
+					if(increase >0){
+						let originalPosition = tempArr2[iterator].at(-1)[1];
+						result[originalPosition] += increase;
+					}
+				}
+			}
+			while(tempArr1[posA].length > 0){
+				tempArr2[iterator].push(tempArr1[posA].shift());
+				increase = tempArr2[iterator].length - (lenPosA - tempArr1[posA].length);
+				if(increase >0){
+					let originalPosition = tempArr2[iterator].at(-1)[1];
+					result[originalPosition] += increase;
+				}
+			}
+			while(tempArr1[posB].length > 0){
+				tempArr2[iterator].push(tempArr1[posB].shift());
+				increase = tempArr2[iterator].length - (lenPosA + (lenPosB - tempArr1[posB].length));
+				if(increase >0){
+					let originalPosition = tempArr2[iterator].at(-1)[1];
+					result[originalPosition] += increase;
+				}
+			}
+
+			if(reverse){
+				tempArr1.pop();
+				tempArr1.pop();
+			} else {
+				tempArr1.shift();
+				t
+			}
+
+			if(tempArr1.length = 1){
+				if(reverse){
+					tempArr2.unshift(tempArr1[0]);
+				} else {
+					tempArr2.push(tempArr1[0]);
+				}
+			}
+
+			// if(reverse){
+			// 	posA -= 2;
+			// 	posB -= 2;
+			// 	iterator --;
+			// } else {
+			// 	posA += 2;
+			// 	posB += 2;
+			// 	iterator ++;
+			// }
+
+		}
+
+	}
+
+	// if(arr.length % 2){
+	// 	let pos1 = tempArr1.length - 1;
+	// 	let pos2 = arr.length -1;
+	// 	tempArr1.push([arr[arr.length-1],arr.length-1,0]);
+	// 	if(tempArr1[pos1][0][0] > arr[pos2]){
+
+	// 		tempArr1[i]=[];
+	// 		tempArr1[i][0]=[arr[pos+1],pos+1,0];
+	// 		result[pos+1] = 0;
+	// 		tempArr1[i][1]=[arr[pos],pos,1];					
+	// 		result[pos] = 1;
+	// 	} else {
+	// 		tempArr1[i]=[];
+	// 		tempArr1[i][0]=[arr[pos],pos,0];
+	// 		result[pos] = 0;
+	// 		tempArr1[i][1]=[arr[pos+1],pos+1,0];
+	// 		result[pos+1] = 0;
+	// 	}
+	// }
+
 };
 
+let test;
 
-
-console.log(solution(test2));
+console.log(mergeSort(testMatrix));
+console.log(smaller(testMatrix));
--- JavaScript Course/node-11-cors/routes/location.js
+++ JavaScript Course/node-11-cors/routes/location.js
@@ -1,32 +1,99 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+
 
 const router = express.Router();
+
+const url = 'mongodb+srv://wyzlicmichal:Profesor1@cluster0.qyurkhn.mongodb.net/locations?retryWrites=true&w=majority';
+const client = new MongoClient(url);//, {useUnifiedTopology: true});
 
 const locationStorage = {
 	locations: []
 };
 
 router.post('/add-location', (req, res, next) => {
-	const id = Math.random();
-	locationStorage.locations.push({
-		id: id,
-		address: req.body.address,
-		coords: { lat: req.body.lat, lng: req.body.lng }
+	client.connect(function(err,client) {
+		const db = client.db('locations');
+		db.collection('user-locations').insertOne({
+			address: req.body.address,
+			coords: { lat: req.body.lat, lng: req.body.lng }
+		}, function(err, r){	
+			console.log(r);		
+			res.json({ message: 'Stored location!', locId: result.insertedId });
+		})
 	});
-	res.json({ message: 'Stored location!', locId: id });
+
+	
+
+	// const id = Math.random();
+	// locationStorage.locations.push({
+	// 	id: id,
+	// 	address: req.body.address,
+	// 	coords: { lat: req.body.lat, lng: req.body.lng }
+	// });
+	// res.json({ message: 'Stored location!', locId: id });
+	// try {
+	// 	await client.connect();
+	// 	const database = client.db('locations');
+	// 	const collection = database.collection('user-locations');
+	// 	// create a document to be inserted
+	// 	const doc = {
+	// 	  address: req.body.address,
+	// 	  coords: { lat: req.body.lat, lng: req.body.lng },
+	// 	};
+	// 	const result = await collection.insertOne(doc);
+	// 	res.json({ message: 'Stored location!', locId: result.insertedId });
+	// 	console.log(
+	// 	  `${result.insertedCount} location documents were inserted with the _id: ${result.insertedId}`
+	// 	);
+	 
+	//   } catch (err) {
+	// 	console.dir(err);
+	//   } finally {
+	// 	// await client.close(); // Commented as throwing some topology errors on mongo connection
+	//   }
 });
 
 router.get('/location/:locId', (req, res, next) => {
-	const locationId = +req.params.locId;
-	const location = locationStorage.locations.find(loc => {
-		return loc.id === locationId;
-	});
-	if(!location){
-		return res.status(404).json({message: 'Not found'});
-	}
-	console.log(location);
-	res.json({address: location.address, coordinates: location.coords})
-
+	// const locationId = +req.params.locId;
+	// const location = locationStorage.locations.find(loc => {
+	// 	return loc.id === locationId;
+	// });
+	// if(!location){
+	// 	return res.status(404).json({message: 'Not found'});
+	// }
+	// console.log(location);
+	// res.json({address: location.address, coordinates: location.coords})
+	// try {
+	// 	await client.connect();
+	// 	const database = client.db('locations');
+	// 	const collection = database.collection('user-locations');
+	 
+	// 	// Query for a movie that has the title 'The Room'
+	// 	const query = {
+	// 	  _id: new mongodb.ObjectId(locationId),
+	// 	};
+	 
+	// 	const location = await collection.findOne(query);
+	// 	// since this method returns the matched document, not a cursor, print it directly
+	// 	console.log(location);
+	 
+	// 	if (!location) {
+	// 	  res.status(404).json('Not Found!');
+	// 	  return;
+	// 	}
+	 
+	// 	res.json({
+	// 	  message: 'Retrieved location!',
+	// 	  coordinates: location.coords,
+	// 	  address: location.address,
+	// 	});
+	 
+	// 	} catch (err) {
+	// 		console.dir(err);
+	// 	} finally {
+	// 	// await client.close(); // Commented as throwing some topology errors on mongo connection
+	// 	}
 });
 
 module.exports = router;
