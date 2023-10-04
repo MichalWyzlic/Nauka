52a5b9f8bd2d21d09202e0eae35276636d326f47
Michal Wyzlic <wyzlic.michal@gmail.com>
2023-10-04 17:40:02 +0200

    React - S21 error page - done

--- React Course/S21-starting-project/my-app/src/App.js
+++ React Course/S21-starting-project/my-app/src/App.js
@@ -1,18 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { createBrowserRouter,  RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
+import RootLayout from './pages/Root';
+import ErrorPage from './pages/Error';
 
 const router = createBrowserRouter([
-	{path: '/', element: <HomePage/>},
-	{path: '/products', element: <ProductsPage/>}
+	{
+		path: '/',
+		errorElement: <ErrorPage/>,
+		element: <RootLayout />,
+		children: [
+			{ path: '/', element: <HomePage />, errorElement: <ErrorPage/>},
+			{ path: '/products', element: <ProductsPage />, errorElement: <ErrorPage/> }
+		]
+	}
 ]);
 
 function App() {
-  return (
-    <RouterProvider router={router}/>
-  );
+	return <RouterProvider router={router} />;
 }
 
 export default App;
