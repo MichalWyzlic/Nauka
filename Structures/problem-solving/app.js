53efa6658de099e9293c906d890a3d9346d55e7c
Michal Wyzlic <wyzlic.michal@gmail.com>
2024-04-29 13:24:49 +0200

    graph remove vertex

--- Structures/problem-solving/app.js
+++ Structures/problem-solving/app.js
@@ -23,4 +23,34 @@ class Graph{
 		index = this.adjacencyList[v2].indexOf(v1);
 		this.adjacencyList[v2].splice(index,1);
 	}
+
+	removeVertex(v1){
+		this.adjacencyList[v1].forEach((element) => {
+			let index = this.adjacencyList[element].indexOf(v1);
+			this.adjacencyList[element].splice(index,1);
+		});		
+		delete this.adjacencyList[v1];		
+	}
 }
+
+
+const myGraph = new Graph;
+
+myGraph.addVertex('Tokyo');
+myGraph.addVertex('Dallas');
+myGraph.addVertex('Aspen');
+myGraph.addVertex('Hong Kong');
+myGraph.addVertex('Los Angeles');
+
+myGraph.addEdge('Tokyo', 'Dallas');
+myGraph.addEdge('Tokyo', 'Hong Kong');
+myGraph.addEdge('Dallas', 'Aspen');
+myGraph.addEdge('Dallas', 'Hong Kong');
+myGraph.addEdge('Dallas', 'Los Angeles');
+myGraph.addEdge('Hong Kong', 'Los Angeles');
+
+console.log(myGraph);
+
+myGraph.removeVertex('Dallas');
+
+console.log(myGraph);
