625f5dad92ce9e9e90f2fc84ce28da5949a4e7da
Michal Wyzlic <wyzlic.michal@gmail.com>
2024-11-04 14:51:24 +0100

    Count words

---
+++ CodeWars/Count words/Test1.js
@@ -0,0 +1,28 @@
+function wordCount(s) {	
+	let tempStr = ' ' + s.toLowerCase().replace(/[^a-z]/g, ' ') + ' ';
+	tempStr = tempStr.replace(/\s+/g, ' ');
+	tempStr = tempStr.replace(/( (a\s*)+ )|( (the\s*)+ )|( (on\s*)+ )|( (of\s*)+ )|( (upon\s*)+ )|( (in\s*)+ )/g, ' ');
+	tempStr = tempStr.replace(/( (as\s*)+ )|( (at\s*)+ )/g, ' ');	
+	tempStr = tempStr.replace(/^\s+/g, '');
+	tempStr = tempStr.replace(/\s+$/g, '');
+	const strArray = tempStr.split(' ');
+	console.log(tempStr);
+	return strArray.length;  
+}
+
+let longText = "I’d been using my sphere as a stool. I traced counterclockwise circles on it with my fingertips and it shrank until I could palm it. My bolt had shifted while I’d been sitting. I pulled it up and yanked the pleats straight as I careered around tables, chairs, globes, and slow-moving fraas. I passed under a stone arch into the Scriptorium. The place smelled richly of ink. Maybe it was because an ancient fraa and his two fids were copying out books there. But I wondered how long it would take to stop smelling that way if no one ever used it at all; a lot of ink had been spent there, and the wet smell of it must be deep into everything."
+    
+console.log('I d been using my sphere stool I traced counterclockwise circles it with my fingertips and it shrank until I could palm it My bolt had shifted while I d been sitting I pulled it up and yanked pleats straight I careered around tables chairs globes and slow moving fraas I passed under stone arch into Scriptorium place smelled richly ink Maybe it was because an ancient fraa and his two fids were copying out books there But I wondered how long it would take to stop smelling that way if no one ever used it all lot ink had been spent there and wet smell it must be deep into everything'.split(' ').length);
+console.log('i d been using my sphere stool i traced counterclockwise circles it with my fingertips and it shrank until i could palm it my bolt had shifted while i d been sitting i pulled it up and yanked pleats straight i careered around tables chairs globes and slow moving fraas i passed under stone arch into scriptorium place smelled richly ink maybe it was because an ancient fraa and his two fids were copying out books there but i wondered how long it would take to stop smelling that way if no one ever used it all lot ink had been spent there and wet smell it must be deep into everything'.split(' ').length);
+
+console.log(wordCount("hello there"));  // 2)
+console.log(wordCount("hello there and a hi"));  // 4)
+console.log(wordCount("I'd like to say goodbye"));  // 6)
+console.log(wordCount("Slow-moving user6463 has been here"));  // 6)
+console.log(wordCount("%^&abc!@# wer45tre"));  // 3)
+console.log(wordCount("abc123abc123abc"));  // 3)
+console.log(wordCount("Really2374239847 long ^&#$&(*@# sequence"));  // 3)
+console.log(wordCount(longText));  // 3)
+//i d been using my sphere stool i traced counterclockwise circles it with my fingertips and it shrank until i could palm it my bolt had shifted while i d been sitting i pulled it up and yanked pleats straight i careered around tables chairs globes and slow moving fraas i passed under stone arch into scriptorium place smelled richly ink maybe it was because an ancient fraa and his two fids were copying out books there but i wondered how long it would take to stop smelling that way if no one ever used it all lot ink had been spent there and wet smell it must be deep into everything
+//I d been using my sphere stool I traced counterclockwise circles it with my fingertips and it shrank until I could palm it My bolt had shifted while I d been sitting I pulled it up and yanked pleats straight I careered around tables chairs globes and slow moving fraas I passed under stone arch into Scriptorium place smelled richly ink Maybe it was because an ancient fraa and his two fids were copying out books there But I wondered how long it would take to stop smelling that way if no one ever used it all lot ink had been spent there and wet smell it must be deep into everything
+
