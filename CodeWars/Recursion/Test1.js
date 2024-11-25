8ac217331f106d84ee960a65666d1fd346ed7928
Michal Wyzlic <wyzlic.michal@gmail.com>
2024-11-25 22:54:06 +0100

    findAllDenominators

--- CodeWars/Recursion/Test1.js
+++ CodeWars/Recursion/Test1.js
@@ -160,99 +160,151 @@
 // console.log(restoreBrackets("[[[Co[de[[wa[rs")); // "[[[Co][de]][[wa][rs]]]"],
 // console.log(restoreBrackets("[[[[[[[codewars")); // "[[[[[[[co]d]e]w]a]r]s]"],
 
-function encode(string) {
-	function helper(str) {
-		if (str.length <= 1) return str;
+// function encode(string) {
+// 	function helper(str) {
+// 		if (str.length <= 1) return str;
 
-		//test for identical characters
-		let i = 1;
-		while (i < str.length) {
-			if (str.charAt(i - 1) != str.charAt(i)) break;
+// 		//test for identical characters
+// 		let i = 1;
+// 		while (i < str.length) {
+// 			if (str.charAt(i - 1) != str.charAt(i)) break;
+// 			i++;
+// 		}
+// 		if (i === str.length) return str.charAt(0);
+
+// 		let midPoint = Math.floor(str.length / 2);
+// 		return (
+// 			'[' +
+// 			helper(str.slice(0, midPoint)) +
+// 			helper(str.slice(midPoint)) +
+// 			']'
+// 		);
+// 	}
+// 	return string.length.toString() + ',' + helper(string);
+// }
+
+// function decode(code) {
+// 	let inputLen = 0;
+// 	let inputStr = '';
+// 	for(let i = 1; i < code.length -1; i++){
+// 		if(code.charAt(i) === ',') {
+// 			inputLen = +code.slice(0,i);
+// 			inputStr = code.slice(i + 1);
+// 			break;
+// 		}
+// 	}
+// 	function helper(str, len) {
+// 		if ((len <= 1)) return str;
+// 		if ((str.length === 1)) return str.repeat(len);
+// 		let midPoint = Math.floor(len / 2);
+// 		// two sides of the string are characters
+// 		if (str.charAt(1) != '[' && str.charAt(str.length - 2) != ']') {
+// 			return (
+// 				str.charAt(1).repeat(midPoint) +
+// 				str.charAt(str.length - 2).repeat(len - midPoint)
+// 			);
+// 		}
+// 		// the left side is an array, the right is a character
+// 		if (str.charAt(1) === '['  && str.charAt(str.length - 2) != ']') {
+// 			return (
+// 				helper(str.slice(1, -2), midPoint) +
+// 				str.charAt(str.length - 2).repeat(len - midPoint)
+// 			);
+// 		}
+// 		// the right side is an array, the left is a character
+// 		if (str.charAt(1) != '[' && str.charAt(str.length - 2) === ']') {
+// 			return (
+// 				str.charAt(1).repeat(midPoint) +
+// 				helper(str.slice(2, -1), len - midPoint)
+// 			);
+// 		}
+// 		// two sides of the string are arrays
+// 		let counter = 0;
+// 		let i;
+// 		for(i = 1; i < str.length -2; i++){
+// 			if(str.charAt(i) === '[') counter++;
+// 			if(str.charAt(i) === ']') counter--;
+// 			if(counter === 0) break;
+// 		}
+// 		return (
+// 			helper(str.slice(1, i + 1), Math.floor(len/2)) + helper(str.slice(i + 1, -1), Math.ceil(len/2))
+// 		);
+// 	}
+// 	return helper(inputStr, inputLen);
+// }
+
+// console.log(encode('')); // "0," );
+// console.log(encode('x')); // "1,x" );
+// console.log(encode('xx')); // "2,x" );
+// console.log(encode('xyy')); // "3,[xy]" );
+// console.log(encode('xyyy')); // "4,[[xy]y]" );
+// console.log(encode('xxxxyyyy')); // "8,[xy]" );
+// console.log(encode('xyyyyyyy')); // "8,[[[xy]y]y]" );
+// console.log(encode('xxxxxxxy')); // "8,[x[x[xy]]]" );
+// console.log(encode('111111222')); // "9,[1[12]]" );
+// console.log(encode('Codewars')); // "8,[[[Co][de]][[wa][rs]]]" );
+// console.log(encode('a,b,c')); // "5,[[a,][b[,c]]]" );
+
+// console.log(decode('0,')); // "" );
+// console.log(decode('1,x')); // "x" );
+// console.log(decode('2,x')); // "xx" );
+// console.log(decode('3,[xy]')); // "xyy" );
+// console.log(decode('4,[[xy]y]')); // "xyyy" );
+// console.log(decode('8,[xy]')); // "xxxxyyyy" );
+// console.log(decode('8,[[[xy]y]y]')); // "xyyyyyyy" );
+// console.log(decode('8,[x[x[xy]]]')); // "xxxxxxxy" );
+// console.log(decode('9,[1[12]]')); // "111111222" );
+// console.log(decode('8,[[[Co][de]][[wa][rs]]]')); // "Codewars" );
+// console.log(decode('5,[[a,][b[,c]]]')); // "a,b,c" );
+
+let primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43];
+
+//find all prime numbers up to the search limit
+function findPrime(searchLimit) {
+	//let searchLimit = Math.floor(Math.sqrt(n));
+	if (searchLimit <= primeNumbers[primeNumbers.length - 1]) return;
+	//next prime candidate
+	let i = primeNumbers[primeNumbers.length - 1] + 4;
+
+	//check if the num is divisible by any number in the primeNumbers table
+	function check(num) {
+		let limit = Math.floor(Math.sqrt(num));
+		for (let i = 0; primeNumbers[i] <= limit; i++) {
+			if (num % primeNumbers[i] === 0) return false;
+		}
+		return true;
+	}
+	while (i < searchLimit) {
+		if (check(i)) primeNumbers.push(i);
+		i += 2;
+		if (check(i)) primeNumbers.push(i);
+		i += 4;
+	}
+}
+
+function findAllDenominators(num) {
+	let searchLimit = Math.floor(Math.sqrt(num));
+	if(searchLimit > primeNumbers[primeNumbers.length-1]) findPrime(searchLimit);
+	let results = [];
+
+	function recursion(num) {
+		if(num === 1) return;
+		let i = 0;
+		while (primeNumbers[i] < searchLimit) {
+			if (num % primeNumbers[i] === 0) {
+				results.push(primeNumbers[i]);
+				recursion(num / primeNumbers[i]);
+				break;
+			}
 			i++;
 		}
-		if (i === str.length) return str.charAt(0);
-
-		let midPoint = Math.floor(str.length / 2);
-		return (
-			'[' +
-			helper(str.slice(0, midPoint)) +
-			helper(str.slice(midPoint)) +
-			']'
-		);
 	}
-	return string.length.toString() + ',' + helper(string);
+
+	recursion(num);
+
+	return results;
 }
 
-function decode(code) {
-	let inputLen = 0;
-	let inputStr = '';
-	for(let i = 1; i < code.length -1; i++){
-		if(code.charAt(i) === ',') {
-			inputLen = +code.slice(0,i);
-			inputStr = code.slice(i + 1);
-			break;
-		}
-	}
-	function helper(str, len) {
-		if ((len <= 1)) return str;
-		if ((str.length === 1)) return str.repeat(len);
-		let midPoint = Math.floor(len / 2);
-		// two sides of the string are characters
-		if (str.charAt(1) != '[' && str.charAt(str.length - 2) != ']') {
-			return (
-				str.charAt(1).repeat(midPoint) +
-				str.charAt(str.length - 2).repeat(len - midPoint)
-			);
-		}
-		// the left side is an array, the right is a character
-		if (str.charAt(1) === '['  && str.charAt(str.length - 2) != ']') {
-			return (
-				helper(str.slice(1, -2), midPoint) +
-				str.charAt(str.length - 2).repeat(len - midPoint)
-			);
-		}
-		// the right side is an array, the left is a character
-		if (str.charAt(1) != '[' && str.charAt(str.length - 2) === ']') {
-			return (
-				str.charAt(1).repeat(midPoint) +
-				helper(str.slice(2, -1), len - midPoint)
-			);
-		}
-		// two sides of the string are arrays
-		let counter = 0;
-		let i;
-		for(i = 1; i < str.length -2; i++){
-			if(str.charAt(i) === '[') counter++;
-			if(str.charAt(i) === ']') counter--;
-			if(counter === 0) break;
-		}
-		return (
-			helper(str.slice(1, i + 1), Math.floor(len/2)) + helper(str.slice(i + 1, -1), Math.ceil(len/2))
-		);
-	}
-	return helper(inputStr, inputLen);
-}
-
-console.log(encode('')); // "0," );
-console.log(encode('x')); // "1,x" );
-console.log(encode('xx')); // "2,x" );
-console.log(encode('xyy')); // "3,[xy]" );
-console.log(encode('xyyy')); // "4,[[xy]y]" );
-console.log(encode('xxxxyyyy')); // "8,[xy]" );
-console.log(encode('xyyyyyyy')); // "8,[[[xy]y]y]" );
-console.log(encode('xxxxxxxy')); // "8,[x[x[xy]]]" );
-console.log(encode('111111222')); // "9,[1[12]]" );
-console.log(encode('Codewars')); // "8,[[[Co][de]][[wa][rs]]]" );
-console.log(encode('a,b,c')); // "5,[[a,][b[,c]]]" );
-
-console.log(decode('0,')); // "" );
-console.log(decode('1,x')); // "x" );
-console.log(decode('2,x')); // "xx" );
-console.log(decode('3,[xy]')); // "xyy" );
-console.log(decode('4,[[xy]y]')); // "xyyy" );
-console.log(decode('8,[xy]')); // "xxxxyyyy" );
-console.log(decode('8,[[[xy]y]y]')); // "xyyyyyyy" );
-console.log(decode('8,[x[x[xy]]]')); // "xxxxxxxy" );
-console.log(decode('9,[1[12]]')); // "111111222" );
-console.log(decode('8,[[[Co][de]][[wa][rs]]]')); // "Codewars" );
-console.log(decode('5,[[a,][b[,c]]]')); // "a,b,c" );
+findPrime(100);
+console.log(primeNumbers);
+console.log(findAllDenominators(362880));
