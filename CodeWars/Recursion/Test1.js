5310ba0ff3db100f72fb16917ad01d077877008a
Michal Wyzlic <wyzlic.michal@gmail.com>
2025-01-03 10:51:33 +0100

    The smallest number with a given number of divisors

--- CodeWars/Recursion/Test1.js
+++ CodeWars/Recursion/Test1.js
@@ -70,87 +70,155 @@
 // 	'Valid guesses cannot wrap around the edges of the board'
 // );
 
-// calculate resistance of circuit
-function calculateResistance(circuit) {
-	// do this if a short circuit is encountered
-	// throw new Error("Short Circuit!");
-	// do this if a broken circuit is encountered
-	// throw new Error("Broken Circuit!");
+// // calculate resistance of circuit
+// function calculateResistance(circuit) {
+// 	// do this if a short circuit is encountered
+// 	// throw new Error("Short Circuit!");
+// 	// do this if a broken circuit is encountered
+// 	// throw new Error("Broken Circuit!");
 
-	function recursion(cir) {
-		let isSeries = cir[0];
-		if (cir.length === 1) return isSeries ? 0 : Infinity;		
-		let value = 0;
-		let resistance = 0;
-		let index = 1;
-		while (index < cir.length) {
-			if (cir[index] instanceof Array) {
-				value = recursion(cir[index]);
-			} else {
-				value = cir[index];
-			}
-			if (isSeries) {
-				if (value === Infinity) {
-					return Infinity;
-				}
-				resistance += value;
-			} else {
-				if (value === 0) {
-					return 0;
-				} else if (value !== Infinity) {
-					resistance += 1 / value;
-				}
-			}
+// 	function recursion(cir) {
+// 		let isSeries = cir[0];
+// 		if (cir.length === 1) return isSeries ? 0 : Infinity;
+// 		let value = 0;
+// 		let resistance = 0;
+// 		let index = 1;
+// 		while (index < cir.length) {
+// 			if (cir[index] instanceof Array) {
+// 				value = recursion(cir[index]);
+// 			} else {
+// 				value = cir[index];
+// 			}
+// 			if (isSeries) {
+// 				if (value === Infinity) {
+// 					return Infinity;
+// 				}
+// 				resistance += value;
+// 			} else {
+// 				if (value === 0) {
+// 					return 0;
+// 				} else if (value !== Infinity) {
+// 					resistance += 1 / value;
+// 				}
+// 			}
 
-			index++;
+// 			index++;
+// 		}
+
+// 		if (isSeries) {
+// 			return resistance;
+// 		} else {
+// 			if (resistance === 0) return Infinity;
+// 			return 1 / resistance;
+// 		}
+// 	}
+
+// 	let value = recursion(circuit);
+// 	if (value === 0) {
+// 		throw new Error('Short Circuit!');
+// 	} else if (value === Infinity) {
+// 		throw new Error('Broken Circuit!');
+// 	}
+
+// 	return value;
+// }
+
+// let testCircuit1 = [
+// 	true, // series
+// 	20, // 20Ω resistor
+// 	[
+// 		false, // parallel
+// 		[
+// 			true, // series
+// 			30, // 30Ω resistor
+// 			40 // 40Ω resistor
+// 		],
+// 		30 // 30Ω resistor
+// 	],
+// 	60 // 60Ω resistor
+// ];
+
+// let testCircuit2 = [
+// 	true, // series
+// 	10, // 10Ω resistor
+// 	[
+// 		false, // parallel
+// 		[
+// 			false // parallel, broken circuit
+// 		],
+// 		[
+// 			false // parallel, broken circuit
+// 		]
+// 	]
+// ];
+
+// console.log(calculateResistance(testCircuit1));
+// console.log(calculateResistance(testCircuit2));
+
+// input: Number, output: BigInt
+const primeNum = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31];
+function findNextPrimeNum() {
+	let sixMul = Math.floor((primeNum[primeNum.length - 1] + 1) / 6);
+	let numToCheck = sixMul * 6 + 1;
+	let higher = true;
+	if (numToCheck === primeNum[primeNum.length - 1]) {
+		numToCheck += 4;
+		sixMul++;
+		higher = false;
+	}
+
+	while (true) {
+		let i = 0;
+		let hasADivisor = false;
+		while (
+			primeNum[i] <= Math.floor(Math.sqrt(numToCheck)) &&
+			!hasADivisor &&
+			i < primeNum.length
+		) {
+			hasADivisor = numToCheck % primeNum[i] === 0;
+			i++;
 		}
-
-		if (isSeries) {
-			return resistance;
+		if (!hasADivisor) break;
+		if (higher) {
+			sixMul++;
+			numToCheck += 4;
+			higher = false;
 		} else {
-			if (resistance === 0) return Infinity;
-			return 1 / resistance;
+			numToCheck += 2;
+			higher = true;
 		}
 	}
 
-	let value = recursion(circuit);
-	if (value === 0) {
-		throw new Error('Short Circuit!');
-	} else if (value === Infinity) {
-		throw new Error('Broken Circuit!');
-	}
-
-	return value;
+	primeNum.push(numToCheck);
 }
 
-let testCircuit1 = [
-	true, // series
-	20, // 20Ω resistor
-	[
-		false, // parallel
-		[
-			true, // series
-			30, // 30Ω resistor
-			40 // 40Ω resistor
-		],
-		30 // 30Ω resistor
-	],
-	60 // 60Ω resistor
-];
+function f(d) {
+	const dDividers = [];
+	let number = d;
+	let i = 0;
+	while (number > 1 && primeNum[i] <= number) {
+		if (number % primeNum[i] === 0) {
+			dDividers.push(primeNum[i]);
+			number /= primeNum[i];
+		} else {
+			i++;
+			if (i >= primeNum.length) findNextPrimeNum();
+		}
+	}
+	console.log(d);
+	console.log(dDividers);
 
-let testCircuit2 = [
-	true, // series
-	10, // 10Ω resistor
-	[
-		false, // parallel
-		[
-			false // parallel, broken circuit
-		],
-		[
-			false // parallel, broken circuit
-		]
-	]
-];
-
-console.log(calculateResistance(testCircuit1));
-console.log(calculateResistance(testCircuit2));
+	function recursion(div){
+		
+	}
+	
+	function calculateValue(div) {
+		let i = 0;
+		let value = BigInt(1);
+		while (i < div.length) {
+			value *= BigInt(primeNum[i]) ** BigInt(div[div.length-1-i] - 1);
+			i++;
+		}
+		return value;
+	}
+}
